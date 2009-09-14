0b5ccedef50fea4d1cc1d77d31b65946adf0aaa5
Linus G Thiel <linus@Linus-G-Thiels-MacBook.local>
2009-09-14 15:24:56 +0200

    Added Pinax (cloned basic_project)

--- apparelrow/settings.py
+++ apparelrow/settings.py
@@ -1,101 +1,82 @@
-##
-## The apparelrow Project
-##
+# -*- coding: utf-8 -*-
+# Django settings for basic pinax project.
 
-import os
+import os.path
+import posixpath
+import pinax
 
-gettext = lambda s: s
+PINAX_ROOT = os.path.abspath(os.path.dirname(pinax.__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-__install_dir = os.path.realpath(os.path.dirname(__file__))
-
+# tells Pinax to use the default theme
+PINAX_THEME = 'default'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-FORCE_SCRIPT_NAME = ''
+
+# tells Pinax to serve media through django.views.static.serve.
+SERVE_MEDIA = DEBUG
 
 ADMINS = (
-    ('Site Admin', 'admin@example.com'),
+    # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-# H&L: Standard setting
-DATABASE_ENGINE = 'sqlite3'                 # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'apparelrow.db'     # Or path to database file if using sqlite3.
-DATABASE_USER = ''                          # Not used with sqlite3.
-DATABASE_PASSWORD = ''                      # Not used with sqlite3.
-DATABASE_HOST = ''                          # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''                          # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'dev.db'       # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+# Local time zone for this installation. Choices can be found here:
+# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+# although not all variations may be possible on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'US/Eastern'
 
-TIME_ZONE = 'Europe/Stockholm'
-
+# Language code for this installation. All choices can be found here:
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
 LANGUAGE_CODE = 'en'
-
 
 SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
-
 USE_I18N = True
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'media')
 
-MEDIA_URL = ''
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+MEDIA_URL = '/site_media/media/'
+
+# Absolute path to the directory that holds static files like app media.
+# Example: "/home/media/media.lawrence.com/apps/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'site_media', 'static')
+
+# URL that handles the static files like app media.
+# Example: "http://media.lawrence.com"
+STATIC_URL = '/site_media/static/'
+
+# Additional directories which hold static files
+STATICFILES_DIRS = (
+    ('apparelrow', os.path.join(PROJECT_ROOT, 'media')),
+    ('pinax', os.path.join(PINAX_ROOT, 'media', PINAX_THEME)),
+)
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/admin_media/'
+ADMIN_MEDIA_PREFIX = posixpath.join(STATIC_URL, "admin/")
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'VNTg_UtKuARrlc0;oY1\8s5IF'
-
-ROOT_URLCONF = 'apparelrow.urls'
-
-TEMPLATE_DIRS = (
-    os.path.join(__install_dir, 'templates'),
-)
-
-
-# FIXME: Specify list of languages
-LANGUAGES = (
-    ('sv', gettext('Swedish')),
-    ('en', gettext('English')),
-)
-
-
-
-# --- DJANGO CMS 2 SETTINGS ---
-CMS_TEMPLATES = (
-    # SET THESE
-    #('your_template.html',   gettext('Your Template')), 
-)
-
-CMS_MEDIA_PATH = '/admin_media/cms/'
-#CMS_DEFAULT_TEMPLATE = 'your_template.html'     # SET THIS
-CMS_PERMISSION = True
-CMS_SOFTROOT = True
-CMS_SHOW_START_DATE = True
-CMS_SHOW_END_DATE = True
-CMS_LANGUAGE_FALLBACK = True
-CMS_DEFAULT_LANGUAGE = 'en'
-CMS_PLACEHOLDER_CONF = {
-    'content': {
-        'plugins': ('TextPlugin'), # SET THESE
-        'name': gettext("Content"),
-    },
-}
-
-CMS_APPLICATIONS_URLS = (
-    # SET THIS 
-    # Lets the CMS handle define pages for 3rd party apps
-    #('your.app.urls', 'Your app'),
-)
-
+SECRET_KEY = 'zb*p6d^l!by6hhugm+^f34m@-yex9c90yz)c_71t=+lxo%mn(3'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -103,53 +84,99 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.load_template_source',
 )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
-    "django.core.context_processors.request",
-
-    "cms.context_processors.media",
-
-    # YOUR CONTEXT PROCESSORS
-)
-
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-
-    'cms.middleware.page.CurrentPageMiddleware',
-    'cms.middleware.user.CurrentUserMiddleware',
-    'cms.middleware.multilingual.MultilingualURLMiddleware',
+    'django_openid.consumer.SessionConsumer',
+    'account.middleware.LocaleMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+    'pinax.middleware.security.HideSensistiveFieldsMiddleware',
 )
 
+ROOT_URLCONF = 'apparelrow.urls'
 
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, "templates"),
+    os.path.join(PINAX_ROOT, "templates", PINAX_THEME),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    
+    "pinax.core.context_processors.pinax_settings",
+    
+    "notification.context_processors.notification",
+    "announcements.context_processors.site_wide_announcements",
+    "account.context_processors.openid",
+    "account.context_processors.account",
+)
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    # included
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.comments',
-    'django.contrib.markup',
-    'tagging',
-
-    # Basic apps
-    'basic.blog',
-    'basic.inlines',
-
-
-    # CMS
-    'cms',
-    'cms.plugins.text',
-
-    # YOUR MODULES HERE        
-
-    'mptt',
-    # Publisher *has* to be last for CMS 2 to work
-    'publisher',
+    'django.contrib.humanize',
+    'pinax.templatetags',
+    
+    # external
+    'notification', # must be first
+    'django_openid',
+    'emailconfirmation',
+    'mailer',
+    'announcements',
+    'pagination',
+    'timezones',
+    'ajax_validation',
+    'uni_form',
+    'staticfiles',
+    
+    # internal (for now)
+    'basic_profiles',
+    'account',
+    'signup_codes',
+    'about',
+    'django.contrib.admin',
 
 )
 
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda o: "/profiles/profile/%s/" % o.username,
+}
 
-from settings_local import *
+MARKUP_FILTER_FALLBACK = 'none'
+MARKUP_CHOICES = (
+    ('restructuredtext', u'reStructuredText'),
+    ('textile', u'Textile'),
+    ('markdown', u'Markdown'),
+    ('creole', u'Creole'),
+)
+WIKI_MARKUP_CHOICES = MARKUP_CHOICES
+
+AUTH_PROFILE_MODULE = 'basic_profiles.Profile'
+NOTIFICATION_LANGUAGE_MODULE = 'account.Account'
+
+ACCOUNT_OPEN_SIGNUP = True
+ACCOUNT_REQUIRED_EMAIL = False
+ACCOUNT_EMAIL_VERIFICATION = False
+
+EMAIL_CONFIRMATION_DAYS = 2
+EMAIL_DEBUG = DEBUG
+CONTACT_EMAIL = "feedback@example.com"
+SITE_NAME = "Pinax"
+LOGIN_URL = "/account/login/"
+LOGIN_REDIRECT_URLNAME = "what_next"
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
--- apparelrow/urls.py
+++ apparelrow/urls.py
@@ -1,17 +1,39 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
+
+from django.contrib import admin
+admin.autodiscover()
+
+from account.openid_consumer import PinaxConsumer
+
+
+if settings.ACCOUNT_OPEN_SIGNUP:
+    signup_view = "account.views.signup"
+else:
+    signup_view = "signup_codes.views.signup"
+
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^apparelrow/', include('apparelrow.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^$', direct_to_template, {
+        "template": "homepage.html",
+    }, name="home"),
+    
+    url(r'^admin/invite_user/$', 'signup_codes.views.admin_invite_user', name="admin_invite_user"),
+    url(r'^account/signup/$', signup_view, name="acct_signup"),
+    
+    (r'^about/', include('about.urls')),
+    (r'^account/', include('account.urls')),
+    (r'^openid/(.*)', PinaxConsumer()),
+    (r'^profiles/', include('basic_profiles.urls')),
+    (r'^notices/', include('notification.urls')),
+    (r'^announcements/', include('announcements.urls')),
+    
+    (r'^admin/(.*)', admin.site.root),
 )
+
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        (r'^site_media/', include('staticfiles.urls')),
+    )
