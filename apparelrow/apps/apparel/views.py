56705ccc503c460ad1703ace08f7547e3ebcfaaf
Linus G Thiel <linus@Linus-G-Thiels-MacBook.local>
2009-10-09 17:55:31 +0200

    Fixed a generic way of rendering django templates in JavaScript (in filter.html currently) Improved search

--- apparelrow/apps/apparel/views.py
+++ apparelrow/apps/apparel/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.utils.translation import ugettext
 from apparel.models import *
 from django.db.models import Q, Max, Min
+from django.template.loader import find_template_source
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from apparel.json import encode
 
 import re
@@ -13,15 +15,14 @@ from pprint import pprint
 
 WIDE_LIMIT = 10 # FIME: Move to application settings fileI
 
-def search(request):
+def search(request, model):
     result = None
-    match  = re.match(r'/apparel/(?:(.+)/)?search', request.path)
     klass  = {
         'products'     : 'Product',
         'manufacturers': 'Manufacturer',
         'categories'   : 'Category',
         'vendors'      : 'Vendor',
-    }.get(match.group(1))
+    }.get(model)
     
     if klass:
         klass  = eval(klass)
@@ -29,6 +30,19 @@ def search(request):
     else:
         raise Exception('No model to search for')
     
+    paginator = Paginator(result, 20) #FIXME: Make results per page configurable
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        paged_result = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paged_result = paginator.page(paginator.num_pages)
+
+    #FIXME: We don't return the paged result because it's not JSON serializable
     return HttpResponse(
         encode(result),
         mimetype='text/json'
@@ -38,7 +52,7 @@ def search(request):
 def wide_search(request):
     query  = request.GET.get('s')
     result = {
-        'products': Product.objects.filter(product_name__icontains=s, description__icontains=query)[:WIDE_LIMIT],
+        'products': Product.objects.filter(product_name__icontains=query, description__icontains=query)[:WIDE_LIMIT],
         'manufacturers': Manufacturer.objects.filter(name__icontains=query)[:WIDE_LIMIT],
         'categories': Category.objects.filter(name__icontains=query)[:WIDE_LIMIT],
         'vendors': Vendor.objects.filter(name__icontains=query)[:WIDE_LIMIT],
@@ -53,6 +67,18 @@ def filter(request):
     pricerange = VendorProduct.objects.aggregate(min=Min('price'), max=Max('price'))
     pricerange['min'] = int(100 * math.floor(float(pricerange['min']) / 100))
     pricerange['max'] = int(100 * math.ceil(float(pricerange['max']) / 100))
+    #FIXME: Create a generic way of getting relevant templates and putting them into the context
+    template_source, template_origin = find_template_source('apparel/fragments/product_small.html')
+    products = Product.objects.all()
+    paginator = Paginator(products, 20) #FIXME: Make number per page configurable
+    try:
+        page = int(request.GET.get('page', '1'))
+    except ValueError:
+        page = 1
+    try:
+        paged_products = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        paged_products = paginator.page(paginator.num_pages)
     result = {
         'categories': Category._tree_manager.all(),
         'manufacturers': Manufacturer.objects.all(),
@@ -63,6 +89,8 @@ def filter(request):
             #FIXME: Maybe do this a little nicer
         },
         'pricerange': pricerange,
+        'products': paged_products,
+        'product_template': template_source,
     }
     return render_to_response('filter.html', result)
 
