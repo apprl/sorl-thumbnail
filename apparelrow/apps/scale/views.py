f60bb0f592f3591b27e61e28cf5359ce9d25eec3
Linus G Thiel <linus@hanssonlarsson.se>
2009-12-09 00:06:47 +0100

    Implemented transparent images for looks

--- apparelrow/apps/scale/views.py
+++ apparelrow/apps/scale/views.py
@@ -5,6 +5,7 @@ from django.core.files.storage import default_storage
 from sorl.thumbnail.main import DjangoThumbnail
 
 import re
+import os
 
 
 def thumb(request, size, path):
@@ -14,9 +15,33 @@ def thumb(request, size, path):
     m = re.match('^(\d+)(?:x(\d+))?$', size)
     size_group = m.groups() if m.group(2) else (m.group(1), m.group(1))
     
-    thumbnail = DjangoThumbnail(path, size_group) 
+    extension = None
+    if request.GET.get('trans'):
+        path, extension = get_transparent(path)
+
+    thumbnail = DjangoThumbnail(path, size_group, extension=extension) 
     
     # FIXME: Is it possible to do a URL re-write here instead of sending back
     # a location header to the client?
     return HttpResponseRedirect(thumbnail.absolute_url)
 
+def get_transparent(path):
+    path_and_file, ext = os.path.splitext(path)
+    newext = 'png'
+    newpath = '%s_transparent%s%s' % (path_and_file, os.extsep, newext)
+    if not default_storage.exists(newpath):
+        make_transparent(path, newpath)
+    return newpath, newext
+
+def make_transparent(path, newpath):
+    from PIL import Image
+    img = Image.open(default_storage.open(path))
+    img = img.convert('RGBA')
+    pixels = img.load()
+    for y in range(img.size[1]):
+        for x in range(img.size[0]):
+            if pixels[x, y] == (255, 255, 255, 255):
+                pixels[x, y] = (255, 255, 255, 0)
+    output = default_storage.open(newpath, 'wb')
+    img.save(output, 'PNG')
+    output.close()
