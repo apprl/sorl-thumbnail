4ab09a414edd4719dc2f1e01b54dc8eee8c0e068
Gunnar Hansson <gunnar@hanssonlarsson.se>
2010-09-23 17:42:34 +0200

    * Add Fifth Avenue feed  * Add ApparelRow XML provider. Fixes #333

---
+++ apparelrow/importer/framework/provider/apparel-xml.py
@@ -0,0 +1,47 @@
+import libxml2
+from xml.sax.saxutils import unescape
+
+from importer.framework.provider import Provider as BaseProvider
+from importer.framework.mapper import DataMapper
+
+
+class ApparelMapper(DataMapper):
+    def map_variations(self):
+        for v in self.record['variations']:
+            if 'color' in v:
+                c = self.map_colors(v['color'])
+                if len(c): v['color'] = c[0]
+
+class Provider(BaseProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=DataMapper
+
+
+    def process(self):
+        doc = libxml2.readFd(self.file.fileno(), None, 'utf8', libxml2.XML_PARSE_NOENT)
+        ctx = doc.xpathNewContext()
+        
+        # FIXME: check version, date and vendor here
+        
+        for p in ctx.xpathEval('//product'):
+            record = dict([
+                (e.name, self.process_text(e.getContent())) 
+                for e in p.xpathEval('./*')
+            ])
+            
+            record['variations'] = []
+            
+            for v in p.xpathEval('./variations/*'):
+                record['variations'].append(
+                    dict([
+                        (a.name, self.process_text( a.content )) 
+                        for a in v.xpathEval('./@*')
+                    ])
+                )
+            
+            mapper = self.mapper(self, record)
+            self.import_data( mapper.translate() )
+    
+    def process_text(self, text):
+        return unicode( unescape( text ), 'utf-8')
