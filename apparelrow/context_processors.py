6294c6fd769929ed413f4960aeddcb1599f5351b
Gunnar Hansson <gunnar@hanssonlarsson.se>
2010-09-30 17:19:58 +0200

    Adding caching support in templates. Note that this isnt finished

--- apparelrow/context_processors.py
+++ apparelrow/context_processors.py
@@ -2,16 +2,16 @@ import logging
 from apparel.views import js_template, get_template_source
 from django.conf import settings as django_settings
 
-def settings(request):
-    return {'django_settings': django_settings }
 
+def cache_vars(request):
+    return {
+        'CACHE_TIMEOUT': django_settings.CACHE_TEMPLATE_TIMEOUT
+    }
 
-def js_templates(request):
-    # FIXME: Move the js_template and get_template_source to a utilities library
-    
+def js_templates(request):    
     return {
         'default_templates': {
-            'product': js_template(get_template_source('apparel/fragments/product_small.html')),
+            'product': js_template(get_template_source('apparel/fragments/product_small.html'), context=RequestContext(request)),
             #'look_search': get_template_source('apparel/fragments/look_search.html'),
             #'manufacturer_search': js_template(get_template_source('apparel/fragments/manufacturer_search.html')),
         },
