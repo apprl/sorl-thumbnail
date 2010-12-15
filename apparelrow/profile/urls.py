c64c4876838aa2822f9e563a0d6d0ab5e10d1389
Gunnar Hansson <gunnar@hanssonlarsson.se>
2010-12-15 16:27:29 +0100

    Query for retrieving most x popular looks for a user. Fixes #388, Refs #380

--- apparelrow/profile/urls.py
+++ apparelrow/profile/urls.py
@@ -5,7 +5,8 @@ from apparel.models import *
 
 urlpatterns = patterns('',
     (r'^$', 'profile.views.profile'),
-    url(r'^(?P<profile>.*)/looks$', view='apparel.views.look_list', name='looks_by_user'),
+    (r'^looks/$', 'profile.views.looks'),
+ #   url(r'^(?P<profile>.*)/looks$', view='apparel.views.look_list', name='looks_by_user'),
  #    (r'^watcher/$', 'watcher.views.manage'),
     (r'^wardrobe/$', 'django.views.generic.simple.direct_to_template', {'template': 'profile/wardrobe.html'}),
 )
--- apparelrow/profile/views.py
+++ apparelrow/profile/views.py
@@ -2,11 +2,12 @@ import logging
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotAllowed, HttpResponsePermanentRedirect
 from django.template import RequestContext
+from django.db import connection
 
 from apparel.decorators import seamless_request_handling
 from apparel.models import *
 from apparel.forms import *
-
+from voting.models import Vote
 from actstream.models import actor_stream
 
 
@@ -25,3 +26,51 @@ def profile(request):
     }
     
     return render_to_response('profile/profile.html', context, context_instance=RequestContext(request))
+
+
+def looks(request):
+    
+    user = request.user
+    
+    context = {
+        'looks': Look.objects.filter(user=user).order_by('-modified'),
+        'popular_looks': get_top_looks(user, 10)
+    }
+    
+    return render_to_response('profile/looks.html', context, context_instance=RequestContext(request))
+
+
+
+
+def get_top_looks(user, limit=10):
+    """
+    Returns a list of objects for the most popular looks for the given user.
+    """
+    
+    # FIXME: This needs the following refactoring
+    #  - Make work with any model, not just Look
+    #  - Support other databases than MySQL
+    #  - Return dictionary rather than objects
+    
+    query = """
+        SELECT 
+            v.object_id, 
+            SUM(v.vote) AS score
+        FROM 
+            %s AS v,
+            %s AS l
+        WHERE
+                v.object_id = l.id
+            AND l.user_id = %%s
+        GROUP BY v.object_id
+        ORDER BY score DESC
+        LIMIT %%s
+    """ % (
+            connection.ops.quote_name(Vote._meta.db_table),
+            connection.ops.quote_name(Look._meta.db_table)
+    )
+    cursor = connection.cursor()
+    cursor.execute(query, [user.id, limit])
+    objects = Look.objects.in_bulk([id for id, score in cursor.fetchall()])
+    
+    return objects.values()
