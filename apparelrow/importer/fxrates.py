76a73fceedbe5b1b5aa8f9160b83232b4d0532ec
gunnar <gunnar@hanssonlarsson.se>
2011-03-10 19:51:42 +0100

    Adding FX rates import command (arfxrates), adding logging to fxrates import process

--- apparelrow/importer/fxrates.py
+++ apparelrow/importer/fxrates.py
@@ -5,6 +5,9 @@ from xml.sax.saxutils import unescape
 from importer.framework import fetcher
 from importer.models import FXRate
 
+
+logger = logging.getLogger('apparel.importer.fxrates')
+
 class FXRateImporter():
     def __init__(self, file=None, url=None, base_currency=None):
         self.file = file
@@ -13,22 +16,28 @@ class FXRateImporter():
     
     def run(self):
         if self.file:
+            logger.info("Refreshing FX rates from local file %s", self.file)
             return self.import_feed(self.file)
         else:
+            logger.info("Refreshing FX rates from URL %s", self.url)
             return self.import_feed(fetcher.fetch(self.url))
         
     def import_fx_rate(self, currency, rate):
         fxrate = None
+        mode   = ""
+        
         try:
             fxrate = FXRate.objects.get(
                 currency=currency, 
                 base_currency=self.base_currency
             )
+            mode = "Updated"
         except FXRate.DoesNotExist:
             fxrate = FXRate(
                 currency=currency, 
                 base_currency=self.base_currency
             )
+            mode = "Created"
         
         # FIXME: Loggin
         if not isinstance(rate, Decimal):
@@ -36,15 +45,22 @@ class FXRateImporter():
                 
         fxrate.rate = rate
         fxrate.save()
+        logger.debug(u"%s %s", mode, fxrate)
         
         return fxrate
     
     def import_feed(self, rss_file):
-        doc = libxml2.readFile(rss_file, 'utf8', libxml2.XML_PARSE_NOENT)
+        doc = None
+        try:
+            doc = libxml2.readFile(rss_file, 'utf8', libxml2.XML_PARSE_NOENT)
+        except libxml2.treeError, e:
+            raise FXRateImporterParseError(e)
+        
         ctx = doc.xpathNewContext()
         
         re_curr = re.compile(r'^([A-Z]{3})/')
         re_rate = re.compile(r'^1.+?=\s*([0-9\.]+)')
+        count   = 0
         
         for item in ctx.xpathEval('//item'):
             currency = re_curr.search(item.xpathEval('./title')[0].getContent())
@@ -54,5 +70,10 @@ class FXRateImporter():
             if not rate: continue
             
             self.import_fx_rate(currency.group(1), rate.group(1))
+            count += 1
         
+        logger.info("%i FX rates refreshed", count)
         return True
+
+class FXRateImporterParseError(Exception):
+    pass
---
+++ apparelrow/importer/management/commands/arfxrates.py
@@ -0,0 +1,53 @@
+import re, datetime, sys, urllib2
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from importer.fxrates import *
+
+class Command(BaseCommand):
+    args = "<name>"
+    help = "Refresh the foreign exchange rates"
+    option_list = BaseCommand.option_list + (
+        make_option('--file',
+            action='store',
+            dest='file',
+            default=None,
+            help='Import the given file instead of fetching from the Internet',
+        ),
+        make_option('--url',
+            action='store',
+            dest='url',
+            help='URL of file to fetch. Defaults to APPAREL_FXRATES_URL',
+            default=None,
+        ),
+        make_option('--base_currency',
+            action='store',
+            dest='base_currency',
+            help='ISO code of base currency to use. Defaults to APPAREL_BASE_CURRENCY',
+            default=None,
+        ),
+    )
+    
+    def handle(self, *args, **options):
+        kwargs = {}
+        if options['file']:
+            kwargs['file'] = options['file']
+        elif options['url']:
+            kwargs['url']  = options['url']
+        else:
+            kwargs['url'] = settings.APPAREL_FXRATES_URL
+        
+        kwargs['base_currency'] = options['base_currency'] or settings.APPAREL_BASE_CURRENCY
+        
+        importer = FXRateImporter(**kwargs)
+        try:
+            importer.run()
+        except urllib2.HTTPError, e:
+            print "Error fetching URL %s: %s" % (importer.url, e)
+        except FXRateImporterParseError, e:
+            print "Error parsing FX rates data: %s" % e
+        else:
+            print "Foreign exchange rates successfully refreshed"
+
