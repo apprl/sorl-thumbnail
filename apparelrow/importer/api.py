958deeb92a57f0e898adaf6f8f214555d9623d07
Linus G Thiel <linus@hanssonlarsson.se>
2011-05-18 13:38:19 +0200

    remove debug trace

--- apparelrow/importer/api.py
+++ apparelrow/importer/api.py
@@ -120,7 +120,6 @@ class API(object):
         Imports the product
         """
         
-        import pdb; pdb.set_trace()
         # Download and store product image 
         fields = {
             'product_name': self.dataset['product']['product-name'],
