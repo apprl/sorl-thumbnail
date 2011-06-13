99aa4606d37b3ef7f5986aa705771733ba54629f
Joel Bohman <joelboh@gmail.com>
2011-06-13 10:19:19 +0200

    added debug_toolbar to development.py.default

--- apparelrow/settings_common.py
+++ apparelrow/settings_common.py
@@ -153,7 +153,6 @@ INSTALLED_APPS = (
     'pagination',
     'mptt',
     'sorl.thumbnail',
-    'debug_toolbar',
         
     'actstream',
     'tagging',
@@ -184,20 +183,6 @@ INSTALLED_APPS = (
     'apparelrow',
 )
 
-#####
-#INTERNAL_IPS = ('127.0.0.1',)
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
-####
 COMMENTS_APP = 'apparel_comments'
 
 # - COMPRESS SETTINGS -
