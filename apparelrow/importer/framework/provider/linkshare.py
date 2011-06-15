7a213b465a66777d441fb74b9cac4168a35dd5e8
Joel Bohman <joelboh@gmail.com>
2011-06-15 12:43:17 +0200

    removes size from description, manufacturer and name in linkeshare mapper

--- apparelrow/importer/framework/provider/linkshare.py
+++ apparelrow/importer/framework/provider/linkshare.py
@@ -1,11 +1,32 @@
 import itertools
+import re
 
 from importer.framework.provider import CSVProvider
 from importer.framework.parser import utils
 from importer.framework.mapper import DataMapper
 
+regex_size = re.compile('^[Ss]ize: .+\. ')
+
+def parse_product_name(record):
+    product_name = record.get('product-name')
+    product_name_parts = product_name.split(',')
+    if len(product_name_parts) > 1:
+        if product_name_parts[-1].strip().lower().startswith('size:'):
+            del product_name_parts[-1]
+
+    # XXX: Should not need to remove manufacturer from product name
+    #manufacturer = record.get('manufacturer') or record.get('brand')
+    #comma_occurences = manufacturer.count(',') + 1
+    #if manufacturer.strip() == ','.join(product_name_parts[-comma_occurences:]).strip():
+        #del product_name_parts[-comma_occurences:]
+
+    return ', '.join(product_name_parts)
+
 class LinkshareMapper(DataMapper):
     genders = {'FEMALE': 'W', 'MALE': 'M', 'WOMEN': 'W', 'MEN': 'M'}
+
+    def get_product_name(self):
+        return parse_product_name(self.record)
 
     def get_variations(self):
         availability = self.get_availability()
@@ -40,10 +61,19 @@ class LinkshareMapper(DataMapper):
         return discount_price if float(discount_price) else self.record.get('retail-price')
 
     def get_manufacturer(self):
-        return self.record.get('manufacturer') or self.record.get('brand')
+        manufacturer = self.record.get('manufacturer') or self.record.get('brand')
+        manufacturer_parts = [m.strip() for m in manufacturer.split(',')]
+        if len(manufacturer_parts) > 1:
+            if len(manufacturer_parts[0]) <= 5:
+                del manufacturer_parts[0]
+
+            return ', '.join(manufacturer_parts)
+
+        return manufacturer
 
     def get_description(self):
         description = self.record.get('long-description') or self.record.get('description')
+        description = regex_size.sub('', description, re.IGNORECASE)
 
         if self.record.get('material'):
             description += " (%s)" % self.record.get('material')
@@ -111,3 +141,9 @@ class Provider(CSVProvider):
             'agegroup',
             'attribute9',
         )
+
+    def should_merge(self, new_record):
+        return parse_product_name(self.record) == parse_product_name(new_record)
+
+    def merge(self, new_record):
+        pass
