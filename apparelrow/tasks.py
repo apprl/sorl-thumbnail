2024496e2f9c1d3822ed6c38f6a22a3a5e508796
Joel Bohman <joelboh@gmail.com>
2011-07-13 12:27:55 +0200

    ignore result from search index update task, also retry once

--- apparelrow/tasks.py
+++ apparelrow/tasks.py
@@ -8,15 +8,14 @@ from celery.task import task
 
 logger = logging.getLogger(__name__)
 
-@task(name='apparelrow.tasks.search_index_update_task', max_retries=1)
+@task(name='apparelrow.tasks.search_index_update_task', max_retries=1, ignore_result=True)
 def search_index_update_task(app_name, model_name, pk, **kwargs):
-    #logger = search_index_update_task.get_logger(**kwargs)
     try:
         model_class = get_model(app_name, model_name)
         instance = model_class.objects.get(pk=pk)
         search_index = site.get_index(model_class)
         logger.info('update solr index for instance %s' % (instance,))
-        # XXX: might fail here?
         search_index.update_object(instance)
     except Exception, exc:
         logger.error(exc)
+        search_index_update_task.retry(exc=exc)
