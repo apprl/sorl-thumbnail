d1d2e0ea74039721c862236023a1ef182aef17ce
Joel Bohman <joelboh@gmail.com>
2011-09-13 22:47:51 +0200

    forgot to import HttpResponseNotFound

--- apparelrow/statistics/views.py
+++ apparelrow/statistics/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 
 from apparelrow.apparel.models import VendorProduct
 from apparelrow.statistics.messaging import send_increment_clicks
@@ -6,7 +6,6 @@ from apparelrow.statistics.messaging import send_increment_clicks
 def product_click_count(request):
     url = request.GET.get('u', None)
     product_id = request.GET.get('i', None)
-    print product_id
     if url and product_id:
         send_increment_clicks(product_id)
         return HttpResponseRedirect(url)
