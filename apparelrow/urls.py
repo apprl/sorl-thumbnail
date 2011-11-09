61b6b3931d0c007e89dc2886ac053c763b4bad54
Joel Bohman <joelboh@gmail.com>
2011-11-09 19:56:50 +0100

    added custom templates for 403/404/500

--- apparelrow/urls.py
+++ apparelrow/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include, handler404, handler500
+from django.conf.urls.defaults import patterns, url, include, handler404, handler403, handler500
 from django.conf import settings
 
 from django.views.generic.simple import direct_to_template
