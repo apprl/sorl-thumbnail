90a61cf6739e73b576bbefff4a67214fef0e97c5
Linus G Thiel <linus@hanssonlarsson.se>
2011-12-07 18:22:14 +0100

    correct mysql privileges

--- etc/mysql.sql
+++ etc/mysql.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE apparelrow;
-CREATE USER 'apparelrow'@'localhost' IDENTIFIED BY 'ashwe3';
-GRANT ALL PRIVILEGES ON apparelrow.* TO 'apparelrow'@'10.250.227.75';
+CREATE USER 'apparelrow'@'ip-10-250-227-75.eu-west-1.compute.internal' IDENTIFIED BY 'ashwe3';
+GRANT ALL PRIVILEGES ON apparelrow.* TO 'apparelrow'@'ip-10-250-227-75.eu-west-1.compute.internal';
