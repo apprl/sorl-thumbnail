40d7160e266553c0c37baac31f40137203004b91
Joel Bohman <joelboh@gmail.com>
2011-12-11 18:31:13 +0100

    added back button product detail page

--- apparelrow/context_processors.py
+++ apparelrow/context_processors.py
@@ -1,5 +1,6 @@
 from django.conf import settings as django_settings
 from django.core.urlresolvers import reverse
+from django.contrib.sites.models import Site
 
 from apparel.utils import get_gender_from_cookie
 
@@ -11,6 +12,7 @@ def exposed_settings(request):
         'GOOGLE_ANALYTICS_ACCOUNT': django_settings.GOOGLE_ANALYTICS_ACCOUNT,
         'GOOGLE_ANALYTICS_DOMAIN': django_settings.GOOGLE_ANALYTICS_DOMAIN,
         'SHORT_LANGUAGES': django_settings.SHORT_LANGUAGES,
+        'CURRENT_DOMAIN': Site.objects.get_current().domain,
     }
 
 def next_redirects(request):
