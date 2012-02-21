fdcadacef2a67da4daf976d826ee18b3ed975929
Joel Bohman <joelboh@gmail.com>
2012-02-21 21:57:40 +0100

    added importer for farfetch feed

---
+++ apparelrow/importer/framework/provider/farfetch.py
@@ -0,0 +1,21 @@
+from importer.framework.provider.linkshare import LinkshareMapper, Provider as LinkshareProvider
+
+# See linkshare.py
+
+class FarfetchMapper(LinkshareMapper): 
+
+    def get_category(self):
+        category = self.record.get('category')
+
+        if self.record.get('secondary-category'):
+            category += ' > %s' % self.record.get('secondary-category')
+
+        if self.record.get('gender'):
+            category = '%s > %s' % (self.record.get('gender'), category)
+
+        return category
+
+class Provider(LinkshareProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=FarfetchMapper
