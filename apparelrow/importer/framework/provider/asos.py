2430afa312fb148324df2400508fa82b751d5762
Joel Bohman <joelboh@gmail.com>
2012-03-22 23:45:21 +0100

    added asos provider

---
+++ apparelrow/importer/framework/provider/asos.py
@@ -0,0 +1,18 @@
+from importer.framework.provider.zanox import ZanoxMapper, Provider as ZanoxProvider
+
+class AsosMapper(ZanoxMapper):
+
+    def get_image_url(self):
+        image = self.record.get('ImageLargeURL')
+        if image:
+            large_image = image.replace('xl.', 'xxl.')
+        else:
+            image = self.record.get('ImageMediumURL', '')
+            large_image = image.replace('l.', 'xxl.')
+
+        return [(large_image, self.IMAGE_LARGE), (image, self.IMAGE_SMALL)]
+
+class Provider(ZanoxProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=AsosMapper
