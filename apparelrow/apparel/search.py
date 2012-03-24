51f0838e8f48e191299666b88698f31ed8a70231
Joel Bohman <joelboh@gmail.com>
2012-03-24 13:28:11 +0100

    fixed more like this search, text field is no longer used

--- apparelrow/apparel/search.py
+++ apparelrow/apparel/search.py
@@ -33,10 +33,11 @@ class ResultContainer:
         self.__dict__.update(entries)
 
 def more_like_this_product(product_id, product_gender, limit):
-    kwargs = {'fq': ['django_ct:apparel.product', 'availability:true', 'gender:%s' % (product_gender,)],
+    kwargs = {'fq': ['django_ct:apparel.product', 'published:true', 'availability:true', 'gender:%s' % (product_gender,)],
               'rows': limit}
+    mlt_fields = ['manufacturer_name', 'category_names', 'product_name', 'color_names', 'description']
     connection = Solr(getattr(settings, 'SOLR_URL', 'http://127.0.0.1:8983/solr/'))
-    result = connection.more_like_this('id:apparel.product.%s' % (product_id,), 'text', **kwargs)
+    result = connection.more_like_this('id:apparel.product.%s' % (product_id,), mlt_fields, **kwargs)
     return result
 
 class ApparelSearch(object):
