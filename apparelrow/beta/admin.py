bcbcdd3cfb16f63d93f2f860335269144cb276b4
Joel Bohman <joelboh@gmail.com>
2012-05-23 18:39:50 +0200

    added search and sort on beta admin

--- apparelrow/beta/admin.py
+++ apparelrow/beta/admin.py
@@ -2,10 +2,23 @@ from beta.models import *
 from django.contrib import admin
 
 admin.site.register(Invite)
-admin.site.register(Invitee)
-admin.site.register(InvitePerUser)
+
+class InviteeAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+    list_display = ['email']
+    ordering = ['email']
+
+admin.site.register(Invitee, InviteeAdmin)
+
+class InvitePerUserAdmin(admin.ModelAdmin):
+    search_fields = ['user__name']
+    list_display = ['user']
+    ordering = ['user__name']
+
+admin.site.register(InvitePerUser, InvitePerUserAdmin)
 
 class InviteRequestAdmin(admin.ModelAdmin):
     list_display = ['email', 'invitee']
+    ordering = ['email']
 
 admin.site.register(InviteRequest, InviteRequestAdmin)
