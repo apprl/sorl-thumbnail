4938cd97c7973b0fc0988d9dd5b479f097a4f970
Joel Bohman <joelboh@gmail.com>
2012-07-12 23:26:01 +0200

    list brand in apparelprofile admin

--- apparelrow/profile/admin.py
+++ apparelrow/profile/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from profile.models import ApparelProfile, NotificationCache
 
 class ApparelProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'slug', 'is_brand', 'language', 'followers_count',)
+    list_display = ('user', 'name', 'brand', 'slug', 'is_brand', 'language', 'followers_count',)
     list_filter = ('is_brand',)
     search_fields = ('name',)
     raw_id_fields = ('user', 'brand')
