aaa61a713462dcc565476c4d6148fddee007c5b6
Linus G Thiel <linus@hanssonlarsson.se>
2012-07-13 16:51:59 +0200

    remove redundant scale app

--- apparelrow/scale/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('',
-    # FIXME: Replace site_media/media with whatever prefix is used in the system
-    (r'^(?P<size>\d+(?:x\d+)?)/?(?:media/)?(?P<path>.+)$', 'scale.views.thumb'),
-)
