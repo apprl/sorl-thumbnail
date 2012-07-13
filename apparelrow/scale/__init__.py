16138a3d8c23226fd158d6576b2f76ed0a401f3a
Linus G Thiel <linus@hanssonlarsson.se>
2012-07-13 16:33:23 +0200

    remove debug statements

--- apparelrow/scale/__init__.py
+++ apparelrow/scale/__init__.py
@@ -2,7 +2,6 @@ import os
 from django.core.files.storage import default_storage
 
 def get_transparent(path):
-    print 'get_transparent'
     path_and_file, ext = os.path.splitext(path)
     newext = 'png'
     newpath = '%s_transparent%s%s' % (path_and_file, os.extsep, newext)
@@ -12,7 +11,6 @@ def get_transparent(path):
 
 def make_transparent(path, newpath):
     from PIL import Image
-    print 'make_transparent'
     img = Image.open(default_storage.open(path))
     img = img.convert('RGBA')
     pixels = img.load()
