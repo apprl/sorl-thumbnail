apprl/sorl-thumbnail

commit bd9cd35d682443a9bb4d95188740ff3d805244cc

updated welcome email + translations

apparelrow/profile/models.py
@@ -235,7 +235,7 @@ def send_welcome_mail(sender, user, **kwargs):
     Send welcome email on user created with email signal.
     """
     if user.email:
-        subject = u'Välkommen till Apprl %(username)s' % {'username': user.first_name}
+        subject = _(u'Welcome to Apprl %(username)s' % {'username': user.first_name}
         body = render_to_string('profile/email_welcome.html')
         send_email_confirm_task.delay(subject, body, user.email)
 

apparelrow/locale/en/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2012-07-14 17:46+0200\n"
+"POT-Creation-Date: 2012-07-14 18:13+0200\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -579,6 +579,11 @@ msgstr ""
 
 #: profile/models.py:90
 msgid "Is this the first visit?"
+msgstr ""
+
+#: profile/models.py:238
+#, python-format
+msgid "Welcome to Apprl %(username)s"
 msgstr ""
 
 #: statistics/models.py:35 statistics/models.py:36
@@ -1806,6 +1811,43 @@ msgstr ""
 
 #: templates/profile/confirm_email_subject.html:1
 msgid "Confirm e-mail address change"
+msgstr ""
+
+#: templates/profile/email_welcome.html:5
+msgid ""
+"At Apprl we have collected clothes, shoes and accessories from all the "
+"leading international brands and retailers in one place."
+msgstr ""
+
+#: templates/profile/email_welcome.html:6
+msgid ""
+"By ♥-ing products and following other members & brands you'll make the site "
+"smarter and more personalized. For example:"
+msgstr ""
+
+#: templates/profile/email_welcome.html:8
+msgid ""
+"Products that you ♥ will be placed under your <a href=\"http://apprl.com/"
+"profile/\">profile</a>. You can create <a href=\"http://apprl.com/looks/"
+"\">looks</a> from these to share on Apprl, Facebook or your blog."
+msgstr ""
+
+#: templates/profile/email_welcome.html:9
+msgid ""
+"You find all the products that you friends ♥ under <a href=\"http://apprl."
+"com/#f=1\">Friends' Favourites</a>."
+msgstr ""
+
+#: templates/profile/email_welcome.html:10
+msgid ""
+"Keep yourself updated when your friends ♥, comment and create looks under <a "
+"href=\"http://apprl.com/home/\">Friends' Updates</a>"
+msgstr ""
+
+#: templates/profile/email_welcome.html:12
+msgid ""
+"If you have questions, feedback or just want to get in touch let us know on "
+"<a href=\"mailto:info@apprl.com\">info@apprl.com</a>!"
 msgstr ""
 
 #: templates/profile/followers.html:14

apparelrow/locale/sv/LC_MESSAGES/django.po
@@ -8,7 +8,7 @@ msgid ""
 msgstr ""
 "Project-Id-Version: PACKAGE VERSION\n"
 "Report-Msgid-Bugs-To: \n"
-"POT-Creation-Date: 2012-07-14 17:46+0200\n"
+"POT-Creation-Date: 2012-07-14 18:13+0200\n"
 "PO-Revision-Date: YEAR-MO-DA HO:MI+ZONE\n"
 "Last-Translator: FULL NAME <EMAIL@ADDRESS>\n"
 "Language-Team: LANGUAGE <LL@li.org>\n"
@@ -588,6 +588,11 @@ msgstr "När någon börjat följa mig"
 #: profile/models.py:90
 msgid "Is this the first visit?"
 msgstr ""
+
+#: profile/models.py:238
+#, python-format
+msgid "Welcome to Apprl %(username)s"
+msgstr "Välkommen till Apprl %(username)s"
 
 #: statistics/models.py:35 statistics/models.py:36
 msgid "Product clicks"
@@ -1913,6 +1918,56 @@ msgstr ""
 msgid "Confirm e-mail address change"
 msgstr "Bekräfta byte av e-postadress"
 
+#: templates/profile/email_welcome.html:5
+msgid ""
+"At Apprl we have collected clothes, shoes and accessories from all the "
+"leading international brands and retailers in one place."
+msgstr ""
+"Hos oss hittar du kläder, skor och accessoarer från de bästa online-"
+"butikerna på ett ställe."
+
+#: templates/profile/email_welcome.html:6
+msgid ""
+"By ♥-ing products and following other members & brands you'll make the site "
+"smarter and more personalized. For example:"
+msgstr ""
+"Genom att ♥ produkter och följa andra medlemmar & varumärken kan du göra "
+"siten smartare och mer personligt anpassad."
+
+#: templates/profile/email_welcome.html:8
+msgid ""
+"Products that you ♥ will be placed under your <a href=\"http://apprl.com/"
+"profile/\">profile</a>. You can create <a href=\"http://apprl.com/looks/"
+"\">looks</a> from these to share on Apprl, Facebook or your blog."
+msgstr ""
+"Produkter du ♥-ar samlas under din <a href=\"http://apprl.com/profile/"
+"\">profil</a>. Av dessa kan du sedan skapa <a href=\"http://apprl.com/looks/"
+"\">looks</a> att dela på Apprl, Facebook eller på din blogg."
+
+#: templates/profile/email_welcome.html:9
+msgid ""
+"You find all the products that you friends ♥ under <a href=\"http://apprl."
+"com/#f=1\">Friends' Favourites</a>."
+msgstr ""
+"Du hittar alla produkter som dina vänner ♥-ar under <a href=\"http://apprl."
+"com/#f=1\">Vänners favoriter</a>."
+
+#: templates/profile/email_welcome.html:10
+msgid ""
+"Keep yourself updated when your friends ♥, comment and create looks under <a "
+"href=\"http://apprl.com/home/\">Friends' Updates</a>"
+msgstr ""
+"Du kan hålla dig uppdaterad när dina vänner ♥-ar, kommenterar och skapar "
+"looks under <a href=\"http://apprl.com/home/\">Vänners uppdateringar</a>."
+
+#: templates/profile/email_welcome.html:12
+msgid ""
+"If you have questions, feedback or just want to get in touch let us know on "
+"<a href=\"mailto:info@apprl.com\">info@apprl.com</a>!"
+msgstr ""
+"Har du frågor eller feedback tar vi gärna emot det på <a href=\"mailto:"
+"info@apprl.com\">info@apprl.com</a>!"
+
 #: templates/profile/followers.html:14
 msgid ""
 "No one is currently following you, but start off by following other members!"
@@ -2435,9 +2490,6 @@ msgstr "Varför mejlet inte skickades"
 
 #~ msgid "Cheap"
 #~ msgstr "Billigast"
-
-#~ msgid "Welcome %(username)s"
-#~ msgstr "Välkommen %(username)s"
 
 #~ msgid "See you!"
 #~ msgstr "Vi ses!"

apparelrow/templates/profile/email_welcome.html
@@ -2,14 +2,14 @@
 <html>
   <head></head>
   <body>
-    <p>Hos oss hittar du kläder, skor och accessoarer från de bästa online-butikerna på ett ställe.</p>
-    <p>Genom att ♥ produkter och följa andra <a href="http://apprl.com/users/popular">medlemmar</a> kan du göra siten smartare och mer personligt anpassad. Exempelvis:</p>
+    <p>{% trans "At Apprl we have collected clothes, shoes and accessories from all the leading international brands and retailers in one place." %}</p>
+    <p>{% trans "By ♥-ing products and following other members & brands you'll make the site smarter and more personalized. For example:" %}</p>
     <ul>
-      <li>Produkter du ♥-ar samlas under din <a href="http://apprl.com/profile/">profil</a>. Av dessa kan du sedan skapa <a href="http://apprl.com/looks/">looks</a> att dela på Apprl, Facebook eller på din blogg.</li>
-      <li>Du hittar alla produkter som dina vänner ♥-ar under <a href="http://apprl.com/#f=1">Vänners favoriter</a>.</li>
-      <li>Du kan hålla dig uppdaterad när dina vänner ♥-ar, kommenterar och skapar looks under <a href="http://apprl.com/home/">Vänners uppdateringar</a>.</li>
+      <li>{% blocktrans %}Products that you ♥ will be placed under your <a href="http://apprl.com/profile/">profile</a>. You can create <a href="http://apprl.com/looks/">looks</a> from these to share on Apprl, Facebook or your blog.{% endblocktrans %}</li>
+      <li>{% blocktrans %}You find all the products that you friends ♥ under <a href="http://apprl.com/#f=1">Friends' Favourites</a>.{% endblocktrans %}</li>
+      <li>{% blocktrans %}Keep yourself updated when your friends ♥, comment and create looks under <a href="http://apprl.com/home/">Friends' Updates</a>{% endblocktrans %}</li>
     </ul>
-    <p>Har du frågor eller feedback tar vi gärna emot det på <a href="mailto:info@apprl.com">info@apprl.com</a>!</p>
+    <p>{% blocktrans %}If you have questions, feedback or just want to get in touch let us know on <a href="mailto:info@apprl.com">info@apprl.com</a>!{% endblocktrans %}</p>
     <p>Have fun!<br />
     /The Apprl team</p>
   </body>

commit 454f873d205f69fa654f2b59833540a6fb781f47

fixed a bug introduced in bd9cd35

apparelrow/profile/models.py
@@ -235,7 +235,7 @@ def send_welcome_mail(sender, user, **kwargs):
     Send welcome email on user created with email signal.
     """
     if user.email:
-        subject = _(u'Welcome to Apprl %(username)s' % {'username': user.first_name}
+        subject = _(u'Welcome to Apprl %(username)s') % {'username': user.first_name}
         body = render_to_string('profile/email_welcome.html')
         send_email_confirm_task.delay(subject, body, user.email)
 