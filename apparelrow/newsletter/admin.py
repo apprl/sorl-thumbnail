61321a9bcfc54b7f5b4ddae1c3b3f49ea537c91c
Joel Bohman <joelboh@gmail.com>
2012-07-15 22:54:54 +0200

    forgot to commit a few files related to the newsletter module

---
+++ apparelrow/newsletter/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from newsletter.models import Newsletter
+
+admin.site.register(Newsletter)
---
+++ apparelrow/newsletter/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Newsletter'
+        db.create_table('newsletter_newsletter', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('email', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, null=True, blank=True)),
+        ))
+        db.send_create_signal('newsletter', ['Newsletter'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Newsletter'
+        db.delete_table('newsletter_newsletter')
+
+
+    models = {
+        'newsletter.newsletter': {
+            'Meta': {'object_name': 'Newsletter'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['newsletter']
