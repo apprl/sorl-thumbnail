ba840da38cf8d1ff597a150e148d86dc6d8539d8
Joel Bohman <joelboh@gmail.com>
2012-07-21 09:47:07 +0200

    removed look tags from the client side

--- apparelrow/apparel/forms.py
+++ apparelrow/apparel/forms.py
@@ -4,11 +4,10 @@ from django.utils.translation import ugettext_lazy as _
 from apparel.models import Look, LookComponent
 
 class LookForm(ModelForm):
-    tags = CharField(widget=Textarea, required=False)
     title = CharField(error_messages={'required': _('You have to name your look before your create it.')})
     class Meta:
         model = Look
-        exclude = ('products', 'user', 'gender', 'popularity')
+        exclude = ('tags', 'products', 'user', 'gender', 'popularity')
 
 class LookComponentForm(ModelForm):
     class Meta:
