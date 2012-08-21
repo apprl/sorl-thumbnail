ce0afd22ab941025d1335bb74f5e2739a98ce121
Joel Bohman <joelboh@gmail.com>
2012-08-21 18:26:53 +0200

    added sneakersnstuff provider

---
+++ apparelrow/importer/framework/provider/sneakersnstuff.py
@@ -0,0 +1,15 @@
+from importer.framework.provider.cj import CJMapper, Provider as CJProvider
+
+class SneakersnstuffMapper(CJMapper):
+
+    def get_image_url(self):
+        image = self.record.get('imageurl', '')
+        large_image = image.replace('external-large', 'zoom')
+
+        return [(large_image, self.IMAGE_LARGE), (image, self.IMAGE_SMALL)]
+
+class Provider(CJProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=SneakersnstuffMapper
+        self.unique_fields = ['product-name']
