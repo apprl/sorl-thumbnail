b0ef31e85c7206f4b85586d2a948dca41be494ab
Linus G Thiel <linus@hanssonlarsson.se>
2012-08-22 17:23:09 +0200

    improved lighttpd configuration, added robots.txt

--- fabfile.py
+++ fabfile.py
@@ -210,6 +210,7 @@ def install_site():
     "Add the virtualhost config file to the webserver's config, activate logrotate"
     require('release', provided_by=[deploy, setup])
     upload_template('etc/%(webserver)s.conf.%(hostname)s' % env, '/etc/%(webserver)s/conf-available/%(project_name)s.conf' % env, context=env, use_sudo=True)
+    upload_template('etc/%(webserver)s.conf.include' % env, '/etc/%(webserver)s/conf-available/%(project_name)s.include.conf' % env, context=env, use_sudo=True)
     with settings(warn_only=True):
         sudo('cd /etc/%(webserver)s/conf-enabled/; ln -sf ../conf-available/%(project_name)s.conf %(project_name)s.conf' % env, pty=True)
 
