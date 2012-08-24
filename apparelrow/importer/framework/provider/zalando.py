f6f2bff03f270034692f1fe46058b4f83c7b532a
Joel Bohman <joelboh@gmail.com>
2012-08-25 00:57:35 +0200

    added zalando provider

---
+++ apparelrow/importer/framework/provider/zalando.py
@@ -0,0 +1,18 @@
+from importer.framework.provider.zanox import ZanoxMapper, Provider as ZanoxProvider
+
+class ZalandoMapper(ZanoxMapper):
+
+    def get_image_url(self):
+        image = self.record.get('ImageLargeURL', '') or self.record.get('ImageMediumURL', '')
+        print image
+        large_image = image.replace('detail', 'large')
+
+        return [(large_image, self.IMAGE_LARGE), (image, self.IMAGE_SMALL)]
+
+    def get_variations(self):
+        return [{'color': c} for c in self.map_colors(self.record.get('ExtraTextTwo', ''))]
+
+class Provider(ZanoxProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=ZalandoMapper
