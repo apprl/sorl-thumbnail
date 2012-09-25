fd5701ce50fdc68cf7a424ce0805caf26f44a25f
Joel Bohman <joelboh@gmail.com>
2012-09-25 14:32:12 +0200

    added import provider for mr porter, net-a-porter and the outnet

---
+++ apparelrow/importer/framework/provider/netaporter.py
@@ -0,0 +1,17 @@
+from importer.framework.provider.linkshare import LinkshareMapper, Provider as LinkshareProvider
+
+# See linkshare.py
+
+class NetAPorterMapper(LinkshareMapper):
+
+    def get_image_url(self):
+        image = self.record.get('image-url', '')
+        large_image = image.replace('_in_l', '_in_xl')
+
+        return [(large_image, self.IMAGE_LARGE), (image, self.IMAGE_SMALL)]
+
+class Provider(LinkshareProvider):
+    def __init__(self, *args, **kwargs):
+        super(Provider, self).__init__(*args, **kwargs)
+        self.mapper=NetAPorterMapper
+        self.unique_fields = ['product-id']
