1874c3cf67949374c68a9fd3f6cbaba379d39ee1
Joel Bohman <joelboh@gmail.com>
2012-10-31 16:26:12 +0100

    brand updates should aggregate directly without going through celery

--- apparelrow/apparel/management/commands/brand_updates.py
+++ apparelrow/apparel/management/commands/brand_updates.py
@@ -1,9 +1,13 @@
 import datetime
+import redis
 
+from django.conf import settings
 from django.db.models.loading import get_model
 from django.core.management.base import BaseCommand
+from django.contrib.contenttypes.models import ContentType
 
 from apparel.models import Brand, Product
+from activity_feed.tasks import aggregate
 
 class Command(BaseCommand):
     args = ''
@@ -17,13 +21,39 @@ class Command(BaseCommand):
         any new products are added. If there are new products the task will
         create a new activity.
         """
+        r = redis.StrictRedis(host=settings.CELERY_REDIS_HOST,
+                              port=settings.CELERY_REDIS_PORT,
+                              db=settings.FEED_REDIS_DB)
+
+        content_type = ContentType.objects.get_for_model(Product)
         for brand in Brand.objects.iterator():
             last_update = brand.last_update
             if last_update is None:
                 last_update = datetime.datetime.now() - datetime.timedelta(days=30)
 
+            last_update = datetime.datetime.now() - datetime.timedelta(days=2)
             for product in Product.valid_objects.filter(date_added__gt=last_update, manufacturer=brand).order_by('-modified').iterator():
-                get_model('activity_feed', 'activity').objects.push_activity(brand.profile, 'add_product', product, product.gender)
+                #get_model('activity_feed', 'activity').objects.push_activity(brand.profile, 'add_product', product, product.gender)
+                # Code below is taken from activity_feed.models and
+                # activity_feed.task because we do not want to use a new redis
+                # connection for every product
+                activity, created = get_model('activity_feed', 'activity') \
+                        .objects.get_or_create(user=brand.profile,
+                                               verb='add_product',
+                                               content_type=content_type,
+                                               object_id=product.pk,
+                                               defaults={'active': True, 'gender': product.gender})
+                if not created and activity.active == False:
+                    activity.active = True
+                    activity.save()
+
+                for followers in get_model('profile', 'follow').objects.followers(activity.user):
+                    aggregate(r, followers, 'M', activity)
+                    aggregate(r, followers, 'W', activity)
+                aggregate(r, None, 'M', activity)
+                aggregate(r, None, 'W', activity)
+                aggregate(r, brand.profile, 'M', activity)
+                aggregate(r, brand.profile, 'W', activity)
 
             brand.last_update = datetime.datetime.now()
             brand.save()
