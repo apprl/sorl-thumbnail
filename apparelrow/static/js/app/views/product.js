f53739fbc094eacea99e00d1fefea4836ff441cb
Joel Bohman <joelboh@gmail.com>
2012-11-30 13:29:46 +0100

    added info/add button on products in look editor

--- apparelrow/static/js/app/views/product.js
+++ apparelrow/static/js/app/views/product.js
@@ -4,20 +4,34 @@ App.Views.Product = Backbone.View.extend({
     template: _.template($('#product_small_template').html()),
 
     events: {
-        'mouseenter img': 'mouseenter',
-        'mouseleave img': 'mouseleave'
+        'mouseenter .image-small, .hover': 'mouseenter',
+        'mouseleave .image-small, .hover': 'mouseleave',
+        'click .btn-info': 'info',
+        'click .btn-add': 'add',
     },
 
-    mouseenter: function() {
-        console.log('hover');
+    mouseenter: function(e) {
+        this.$el.find('.hover').show();
     },
 
-    mouseleave: function() {
-        console.log('leave');
+    mouseleave: function(e) {
+        this.$el.find('.hover').hide();
+    },
+
+    info: function(e) {
+        e.preventDefault();
+
+        App.Events.trigger('look_edit:product:info', this.model);
+    },
+
+    add: function(e) {
+        e.preventDefault();
+
+        App.Events.trigger('look_edit:product:add', this.model);
     },
 
     render: function() {
-        this.$el.append(this.template(this.model.toJSON()));
+        this.$el.html(this.template(this.model.toJSON()));
 
         return this;
     }
