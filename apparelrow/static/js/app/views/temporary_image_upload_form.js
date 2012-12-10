ea5489534283608a70ab4eb4cf1d7fc4dd6203c6
Joel Bohman <joelboh@gmail.com>
2012-12-10 09:52:18 +0100

    added loading indicator for look editor photo upload

--- apparelrow/static/js/app/views/temporary_image_upload_form.js
+++ apparelrow/static/js/app/views/temporary_image_upload_form.js
@@ -1,6 +1,13 @@
 App.Views.TemporaryImageUploadForm = Backbone.View.extend({
 
+    tagName: 'div',
+    id: 'image-form',
     template: _.template($('#temporary_image_form_template').html()),
+
+    upload_start: function(e, data) {
+        this.$el.find('.loading').show();
+        this.$el.find('form').hide();
+    },
 
     upload_complete: function(e, data) {
         this.model.set('image', data.result[0].url);
@@ -8,8 +15,9 @@ App.Views.TemporaryImageUploadForm = Backbone.View.extend({
     },
 
     render: function() {
-        this.setElement(this.template());
+        this.$el.html(this.template());
         this.$el.fileupload({
+            submit: _.bind(this.upload_start, this),
             done: _.bind(this.upload_complete, this)
         })
 
