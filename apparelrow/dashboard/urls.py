f071089e8d4152e5daa27f866b1339e78cbd3aa5
Joel Bohman <joelboh@gmail.com>
2013-01-04 00:43:35 +0100

    display dashboard graph per month

--- apparelrow/dashboard/urls.py
+++ apparelrow/dashboard/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'dashboard.views.dashboard', name='dashboard'),
     url(r'^info/$', 'dashboard.views.dashboard_info', name='dashboard-info'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/', 'dashboard.views.dashboard', name='dashboard-date'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/', 'dashboard.views.dashboard', name='dashboard-date'),
 )
--- apparelrow/dashboard/views.py
+++ apparelrow/dashboard/views.py
@@ -1,3 +1,6 @@
+import datetime
+import calendar
+
 from django.shortcuts import render
 from django.db.models import Sum
 
@@ -6,11 +9,29 @@ from dashboard.models import Sale
 
 def dashboard(request, year=None, month=None):
     if request.user.is_authenticated() and request.user.get_profile().is_partner:
-        sales = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID).order_by('sale_date')
+        if year is not None and month is not None:
+            start_date = datetime.date(int(year), int(month), 1)
+        else:
+            start_date = datetime.date.today().replace(day=1)
 
-        sales_total = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID).aggregate(total=Sum('commission'))
+        end_date = start_date
+        end_date = end_date.replace(day=calendar.monthrange(start_date.year, start_date.month)[1])
 
-        return render(request, 'dashboard/partner.html', {'sales': sales,
+        data_per_month = {}
+        for day in range(1, (end_date - start_date).days + 2):
+            data_per_month[start_date.replace(day=day)] = 0
+
+        for sale in Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID) \
+                                .filter(sale_date__gte=start_date, sale_date__lte=end_date) \
+                                .order_by('sale_date') \
+                                .values('sale_date', 'commission'):
+            data_per_month[sale['sale_date'].date()] += sale['commission']
+
+        #sales = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID).order_by('sale_date')
+        sales_total = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID) \
+                                  .aggregate(total=Sum('commission'))
+
+        return render(request, 'dashboard/partner.html', {'sales': data_per_month,
                                                           'sales_total': sales_total['total']})
 
     return render(request, 'dashboard/partner_signup.html')
