8f64b25b811832599320f83b5380e39b5bf31201
Joel Bohman <joelboh@gmail.com>
2013-01-03 21:39:51 +0100

    added dashboard pages except settings

---
+++ apparelrow/dashboard/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'dashboard.views.dashboard', name='dashboard'),
+    url(r'^info/$', 'dashboard.views.dashboard_info', name='dashboard-info'),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/', 'dashboard.views.dashboard', name='dashboard-date'),
+)
--- apparelrow/dashboard/views.py
+++ apparelrow/dashboard/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.shortcuts import render
+from django.db.models import Sum
+
+from dashboard.models import Sale
+
+
+def dashboard(request, year=None, month=None):
+    if request.user.is_authenticated() and request.user.get_profile().is_partner:
+        sales = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID).order_by('sale_date')
+
+        sales_total = Sale.objects.filter(status__gte=Sale.PENDING, status__lt=Sale.PAID).aggregate(total=Sum('commission'))
+
+        return render(request, 'dashboard/partner.html', {'sales': sales,
+                                                          'sales_total': sales_total['total']})
+
+    return render(request, 'dashboard/partner_signup.html')
+
+
+def dashboard_info(request):
+    return render(request, 'dashboard/info.html')
