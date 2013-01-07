22cbb17400fc67f809c745a082fae25ac402a4eb
Joel Bohman <joelboh@gmail.com>
2013-01-07 01:52:56 +0100

    added more info popup to dashboard

--- apparelrow/dashboard/urls.py
+++ apparelrow/dashboard/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^$', 'dashboard.views.dashboard', name='dashboard'),
     url(r'^complete/$', 'dashboard.views.dashboard_complete', name='dashboard-complete'),
     url(r'^info/$', 'dashboard.views.dashboard_info', name='dashboard-info'),
+    url(r'^info/more/$', 'dashboard.views.dashboard_more_info', name='dashboard-more-info'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/', 'dashboard.views.dashboard', name='dashboard-date'),
 )
