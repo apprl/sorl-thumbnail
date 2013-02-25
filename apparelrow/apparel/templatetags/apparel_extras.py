d72115688323624c89ba6f5b3e3830721b80840f
Joel Bohman <joelboh@gmail.com>
2013-02-25 23:34:42 +0100

    updated login panel with a fixed position panel that is always visible when scrolling on all pages

--- apparelrow/apparel/templatetags/apparel_extras.py
+++ apparelrow/apparel/templatetags/apparel_extras.py
@@ -40,22 +40,6 @@ def facebook_button(context, text=None, next=None):
     return dict(next=next, text=text, facebook_icon=facebook_icon, request=context['request'])
 
 
-@register.inclusion_tag('apparel/tags/apparel_facebook_button.html', takes_context=True)
-def apparel_facebook_button(context, image=None):
-    if not 'request' in context:
-        raise AttributeError, 'Please add the ``django.core.context_processors.request`` context processors to your settings.TEMPLATE_CONTEXT_PROCESSORS set'
-    logged_in = context['request'].user.is_authenticated()
-    if 'next' in context:
-        next = context['next']
-    else:
-        next = None
-    if image is not None:
-        button = staticfiles_storage.url('images/' + image)
-    else:
-        button = staticfiles_storage.url('images/fblogo.png')
-    return dict(next=next, logged_in=logged_in, button=button, request=context['request'])
-
-
 @register.filter
 def category_descendants_id(category, include_self=True):
     """
--- apparelrow/apparel/urls.py
+++ apparelrow/apparel/urls.py
@@ -105,7 +105,6 @@ urlpatterns = patterns('',
     url(r'^dialog/like-product/$', 'apparel.views.dialog_like_product', name='dialog-like-product'),
     url(r'^dialog/like-look/$', 'apparel.views.dialog_like_look', name='dialog-like-look'),
     url(r'^dialog/follow-user/$', 'apparel.views.dialog_follow_user', name='dialog-follow-user'),
-    url(r'^dialog/why-facebook$', TemplateView.as_view(template_name='apparel/fragments/dialog_why_facebook.html'), name='dialog-why-facebook'),
     url(r'^dialog/user-feed/$', 'activity_feed.views.dialog_user_feed', name='dialog-user-feed'),
 
     # Embed
