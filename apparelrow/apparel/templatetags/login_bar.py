57c8b1870f5fbbffc69ae21396a08930aa4bbcde
Joel Bohman <joelboh@gmail.com>
2013-03-19 12:37:22 +0100

    added login bar templatetag

---
+++ apparelrow/apparel/templatetags/login_bar.py
@@ -0,0 +1,16 @@
+from django.template import Library
+from django.utils.translation import ugettext
+
+register = Library()
+
+@register.inclusion_tag('apparel/tags/login_bar.html')
+def login_bar(request_path):
+    text = ugettext('Follow our stylemakers, bloggers & brands to discover fashion from the world\'s best online stores.')
+    disabled = False
+
+    if request_path.startswith('/product'):
+        text = ugettext('Sign up to get sale alerts when the price on products you like.')
+    elif request_path.startswith('/accounts') or request_path.startswith('/partner'):
+        disabled = True
+
+    return {'text': text, 'disabled': disabled}
