748ae0cc1586f1fe1e56900985a3ba7a0e026373
Joel Bohman <joelboh@gmail.com>
2013-04-12 11:46:14 +0200

    transactions imported from cj should be marked confirmed if they are closed

--- apparelrow/dashboard/importer/cj.py
+++ apparelrow/dashboard/importer/cj.py
@@ -37,7 +37,7 @@ class Importer(BaseImporter):
                     data_row['original_currency'] = 'EUR'
                     data_row['original_amount'] = row['sale-amount']
                     data_row['user_id'], data_row['placement'] = self.map_placement_and_user(row['sid'])
-                    if row['action-status'] == 'locked':
+                    if row['action-status'] == 'locked' or row['action-status'] == 'closed':
                         data_row['status'] = Sale.CONFIRMED
                     else:
                         data_row['status'] = Sale.PENDING
