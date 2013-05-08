ecbf10c92bd43d5876b4e3fe188255a563ad76e6
Joel Bohman <joelboh@gmail.com>
2013-05-08 09:34:51 +0200

    remove size from product name from provider liberty

--- apparelrow/importer/framework/provider/liberty.py
+++ apparelrow/importer/framework/provider/liberty.py
@@ -2,6 +2,19 @@ from apparelrow.importer.framework.provider.affiliatewindow import AffiliateWind
 
 class LibertyMapper(AffiliateWindowMapper):
 
+    def get_product_name(self):
+        name = super(LibertyMapper, self).get_product_name()
+
+        product_name_parts = name.split(' ')
+        for index, part in enumerate(reversed(product_name_parts)):
+            if part.lower() == 'size' and index <= 2:
+                return ' '.join(product_name_parts[:-(index + 1)])
+
+            elif index > 2:
+                break
+
+        return name
+
     def get_image_url(self):
         image = self.record.get('merchant_image_url', '')
         large_image = image.replace('large1', 'enlarge')
