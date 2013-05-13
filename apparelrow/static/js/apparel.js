16d234dca3bfcd4b4c66c76eb63627899fced101
Joel Bohman <joelboh@gmail.com>
2013-05-13 14:49:48 +0200

    fixed a bug with parsing of comment post result

--- apparelrow/static/js/apparel.js
+++ apparelrow/static/js/apparel.js
@@ -225,7 +225,7 @@ jQuery(document).ready(function() {
             success: function(response, statusText, req) {
                 form.find('textarea').val('');
                 form.find('button').hide();
-                var newComment = jQuery(response).find('.comments-list li').last()
+                var newComment = jQuery(jQuery.parseHTML(response)).find('.comments-list li').last()
                 newComment.appendTo(form.siblings('.comments-list')).slideDown('fast');
                 increase_counts(jQuery('a.comments > span.count'));
             }
