03dd7ae0a538013c5fd6d3e0b90f841295b6eb76
Joel Bohman <joelboh@gmail.com>
2013-08-18 22:04:20 +0200

    update store page and publisher page

--- apparelrow/dashboard/urls.py
+++ apparelrow/dashboard/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^dashboard/$', 'apparelrow.dashboard.views.dashboard', name='dashboard'),
     url(r'^complete/$', 'apparelrow.dashboard.views.dashboard_complete', name='dashboard-complete'),
     url(r'^info/$', 'apparelrow.dashboard.views.dashboard_info', name='dashboard-info'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/', 'apparelrow.dashboard.views.dashboard', name='dashboard-date'),
+    url(r'^dashboard/(?P<year>\d{4})/(?P<month>\d{1,2})/', 'apparelrow.dashboard.views.dashboard', name='dashboard-date'),
 
     url(r'admin/$', 'apparelrow.dashboard.views.dashboard_admin', name='dashboard-admin'),
     url(r'admin/(?P<year>\d{4})/(?P<month>\d{1,2})/$', 'apparelrow.dashboard.views.dashboard_admin', name='dashboard-admin-date'),
