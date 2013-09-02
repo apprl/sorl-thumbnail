45568744e5f0c70ef173058d1b95d3b88b180992
Joel Bohman <joelboh@gmail.com>
2013-09-02 08:59:39 +0200

    only display the finalist on the contest toplist page

--- apparelrow/apparel/views/__init__.py
+++ apparelrow/apparel/views/__init__.py
@@ -796,7 +796,7 @@ def contest_stylesearch_charts(request):
                                                           .filter(likes__created__lte=end_date, likes__active=True) \
                                                           .annotate(num_likes=Count('likes')) \
                                                           .select_related('user') \
-                                                          .order_by('-num_likes', 'created')[:20]
+                                                          .order_by('-num_likes', 'created')[:10]
 
     return render(request, 'apparel/contest_stylesearch_charts.html', {'looks': looks})
 
