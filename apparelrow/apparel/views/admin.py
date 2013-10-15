2313432ad492323071d798a2f5e419d11145e7ed
Joel Bohman <joelboh@gmail.com>
2013-10-15 19:28:19 +0200

    added a table with stats per store and averages for affiliate networks

--- apparelrow/apparel/views/admin.py
+++ apparelrow/apparel/views/admin.py
@@ -149,3 +149,74 @@ def kpi_dashboard(request):
         return render(request, 'apparel/admin/kpi_dashboard.html', context)
 
     raise Http404
+
+
+def stores(request):
+    if request.user.is_authenticated() and request.user.is_superuser:
+        decimal.setcontext(decimal.ExtendedContext)
+
+        # Models
+        ProductStat = get_model('statistics', 'ProductStat')
+        Sale = get_model('dashboard', 'Sale')
+        Vendor = get_model('apparel', 'Vendor')
+        VendorFeed = get_model('importer', 'VendorFeed')
+
+        # Date
+        is_month = bool(request.GET.get('month', True))
+        date = request.GET.get('date')
+        if date:
+            date = datetime.datetime(*[int(x) for x in date.split('-')])
+        else:
+            date = datetime.datetime.now()
+
+        date_start, date_end = get_date_interval(is_month, date, last=False)
+
+        context = {'date_start': date_start, 'date_end': date_end}
+
+        # Group vendors by affiliate
+        context['affiliates'] = {}
+        for vendor in Vendor.objects.all():
+            try:
+                affiliate_name = vendor.vendor_feed.provider_class
+                if affiliate_name not in context['affiliates']:
+                    context['affiliates'][affiliate_name] = {'vendors': [], 'clicks': 0, 'sales': 0, 'sales_sum': 0, 'commission': 0}
+
+                vendor_data = {'vendor_object': vendor}
+
+                # Clicks
+                vendor_data['clicks'] = decimal.Decimal(ProductStat.objects.filter(vendor=vendor, created__range=(date_start, date_end)).count())
+                context['affiliates'][affiliate_name]['clicks'] += vendor_data['clicks']
+
+                # Sales
+                sales_query = Sale.objects.filter(vendor=vendor, created__range=(date_start, date_end), status__range=(Sale.PENDING, Sale.CONFIRMED), is_promo=False, is_referral_sale=False)
+                vendor_data['sales'] = decimal.Decimal(sales_query.count())
+                vendor_data['sales_sum'] = sales_query.aggregate(Sum('converted_amount')).get('converted_amount__sum') or ZERO_DECIMAL
+                context['affiliates'][affiliate_name]['sales'] += vendor_data['sales']
+                context['affiliates'][affiliate_name]['sales_sum'] += vendor_data['sales_sum']
+
+                # Commission
+                commission_query = Sale.objects.filter(vendor=vendor, created__range=(date_start, date_end), status__range=(Sale.PENDING, Sale.CONFIRMED), is_promo=False, is_referral_sale=False)
+                vendor_data['commission'] = commission_query.aggregate(Sum('converted_commission')).get('converted_commission__sum') or ZERO_DECIMAL
+                context['affiliates'][affiliate_name]['commission'] += vendor_data['commission']
+
+                # Actual commission
+                vendor_data['actual_commission'] = vendor_data['commission'] / vendor_data['sales_sum'] * 100
+                context['affiliates'][affiliate_name]['actual_commission'] = context['affiliates'][affiliate_name]['commission'] / context['affiliates'][affiliate_name]['sales_sum'] * 100
+
+                # Conversion
+                vendor_data['conversion'] = vendor_data['sales'] / vendor_data['clicks'] * 100
+                context['affiliates'][affiliate_name]['conversion'] = context['affiliates'][affiliate_name]['sales'] / context['affiliates'][affiliate_name]['clicks'] * 100
+
+                # EPC
+                vendor_data['epc'] = vendor_data['commission'] / vendor_data['clicks']
+                context['affiliates'][affiliate_name]['epc'] = context['affiliates'][affiliate_name]['commission'] / context['affiliates'][affiliate_name]['clicks']
+
+                # Set vendor data
+                context['affiliates'][affiliate_name]['vendors'].append(vendor_data)
+
+            except VendorFeed.DoesNotExist:
+                pass
+
+        return render(request, 'apparel/admin/stores.html', context)
+
+    raise Http404
