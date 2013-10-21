01d69fc2b0750dadb92222b8501e410fb52fa9c6
Joel Bohman <joelboh@gmail.com>
2013-10-21 20:21:34 +0200

    added delete unmapped vendor categories switch to the management command

--- apparelrow/apparel/management/commands/cleanup_vendorcategory.py
+++ apparelrow/apparel/management/commands/cleanup_vendorcategory.py
@@ -1,18 +1,28 @@
+from optparse import make_option
+
 from django.core.management.base import BaseCommand
 
 from apparelrow.apparel.models import VendorCategory
 
+
 class Command(BaseCommand):
     args = ''
     help = ''
-    option_list = BaseCommand.option_list
+    option_list = BaseCommand.option_list + (
+        make_option('--delete-unmapped',
+            action='store_true',
+            dest='delete_unmapped',
+            default=False,
+            help='Delete unmapped vendor categories',
+        ),
+    )
 
     def handle(self, *args, **options):
         deleted_count = 0
         for vc in  VendorCategory.objects.filter(category__isnull=True,
                                                  override_gender__isnull=True,
                                                  default_gender__isnull=True).iterator():
-            if vc.vendor_products.count() == 0:
+            if vc.vendor_products.count() == 0 or options['delete_unmapped']:
                 vc.delete()
                 deleted_count += 1
 
