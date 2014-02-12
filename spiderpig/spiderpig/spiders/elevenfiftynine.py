c6e20b2b9e01ff47a183b75128fa4a046e4ec0e9
Joel Bohman <joelboh@gmail.com>
2014-02-12 20:16:23 +0100

    fixed elevenfiftynine spider

--- spiderpig/spiderpig/spiders/elevenfiftynine.py
+++ spiderpig/spiderpig/spiders/elevenfiftynine.py
@@ -13,6 +13,10 @@ class ElevenfiftynineSpider(XMLFeedSpider, AffiliateMixin):
     def parse_node(self, response, node):
         in_stock = node.xpath('active/text()').extract()[0]
 
+        description = node.xpath('descriptions/descriptions-en/description-en/text()').extract()[0]
+        if not description:
+            description = node.xpath('descriptions/descriptions-sv/description-sv/text()').extract()[0]
+
         l = ProductLoader(item=Product(), selector=node)
         l.add_xpath('key', 'product_url/text()')
         l.add_xpath('sku', 'supplier_reference/text()')
@@ -21,7 +25,7 @@ class ElevenfiftynineSpider(XMLFeedSpider, AffiliateMixin):
         l.add_xpath('url', 'product_url/text()')
         l.add_value('affiliate', self.AFFILIATE_AAN)
         l.add_xpath('category', 'default_category/category_default_name-en/text()')
-        l.add_xpath('description', 'descriptions/descriptions-en/description-en/text()')
+        l.add_value('description', description)
         l.add_xpath('brand', 'manufacturer_name/text()')
         l.add_value('gender', 'U')
         l.add_xpath('colors', 'descriptions/descriptions-en/name-en/text()')
