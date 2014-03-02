c0b6ec923f9a004ad0e5b74e663af44ea51579ca
Joel Bohman <joelboh@gmail.com>
2014-03-02 14:35:50 +0100

    increase gunicorn process by one

--- fabfile.py
+++ fabfile.py
@@ -86,7 +86,7 @@ def production_web():
     env.settings = 'production'
     env.celery_processes='4'
     env.celery_processes_background='3'
-    env.gunicorn_processes='3'
+    env.gunicorn_processes='4'
 
 def staging():
     """
