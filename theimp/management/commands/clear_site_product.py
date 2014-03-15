b7d57b56b49d6bafb777fdeca643bab7d73a0aba
Joel Bohman <joelboh@gmail.com>
2014-03-15 22:20:37 +0100

    added clear site product management command

---
+++ theimp/management/commands/clear_site_product.py
@@ -0,0 +1,29 @@
+import json
+from optparse import make_option
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.loading import get_model
+
+from theimp.utils import ProductItem
+
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Clear site product'
+    option_list = BaseCommand.option_list + (
+        make_option('--vendor',
+            action='store',
+            dest='vendor',
+            default=None,
+            help='Run only this vendor',
+        ),
+    )
+
+    def handle(self, *args, **options):
+        vendor = get_model('theimp', 'Vendor').objects.get(name=options.get('vendor'))
+        products = get_model('theimp', 'Product').objects.filter(vendor__name=options.get('vendor'))
+        for product in products:
+            item = ProductItem(product)
+            item.set_site_product(None)
+            product.json = json.dumps(item.data)
+            product.save()
