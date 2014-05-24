d9bcabb53de4b046459da290d8540d42ce11f29a
Klas <Klas79@gmail.com>
2014-05-24 06:33:51 +0200

    Removing temp conf for v2

--- etc/v2/gunicorn-admin.conf.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-import multiprocessing
-
-bind = "127.0.0.1:%(gunicorn_admin_port)s"
-workers = "%(gunicorn_admin_processes)s"
-loglevel = "error"
-proc_name = "%(project_name)s-admin"
