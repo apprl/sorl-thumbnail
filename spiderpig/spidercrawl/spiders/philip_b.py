771e2fccda3084c3d3cd0167f8842502dd8a4b69
Klas <Klas79@gmail.com>
2014-07-31 20:25:50 +0200

    Added PhilipB crawler

---
+++ spiderpig/spidercrawl/spiders/philip_b.py
@@ -0,0 +1,34 @@
+from scrapy.contrib.spiders import CSVFeedSpider
+
+from spiderpig.spidercrawl.items import Product
+from spiderpig.spidercrawl.spiders import AffiliateMixin
+
+
+class PhilipBSpider(CSVFeedSpider, AffiliateMixin):
+    name = 'philip-b'
+    allowed_domains = ['www.philipb.com']
+    start_urls = ['http://www.philipb.com/products.txt']
+    delimiter = '\t'
+
+    def parse_row(self, response, row):
+        product_url = row.get('Product URL').strip()
+        item = Product()
+        item['key'] = product_url
+        item['sku'] = row.get('Product ID')
+        item['name'] = row.get('Product Name')
+        item['vendor'] = self.name
+        item['url'] = product_url
+        item['affiliate'] = self.AFFILIATE_AAN
+        item['category'] = row.get('Product Category')
+        item['description'] = row.get('Product Text Description')
+        item['brand'] = row.get('Brand Name')
+        item['gender'] = row.get('Product Category')
+        item['colors'] = ''
+        item['regular_price'] = row.get('Price')
+        item['discount_price'] = ''
+        item['currency'] = 'USD'
+        item['in_stock'] = True
+        item['stock'] = ''
+        item['image_urls'] = [row.get('Image URL')]
+
+        return item
