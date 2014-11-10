937541d0b00f83ac6430737ef567ab5fd8eebcc0
Klas <klas@apprl.com>
2014-11-10 19:14:19 +0100

    Added debug printouts

--- apparelrow/apparel/management/commands/invalidate_embed_cache.py
+++ apparelrow/apparel/management/commands/invalidate_embed_cache.py
@@ -18,10 +18,14 @@ class Command(BaseCommand):
         # Empty shop embed
         for user_id in get_user_model().objects.values_list('pk', flat=True):
             for x in itertools.product((x[0] for x in settings.LANGUAGES), ['A', 'M', 'W']):
-                cache.delete(reverse('shop-embed', args=[user_id, x[0], x[1]]))
+                key = reverse('shop-embed', args=[user_id, x[0], x[1]])
+                cache.delete(key)
+                print 'Deleting %s' % key
 
         # Empty look embed
         for look_slug in get_model('apparel', 'Look').objects.values_list('slug', flat=True):
             cache.delete(reverse('look-embed', args=[look_slug]))
             for look_embed in get_model('apparel', 'LookEmbed').objects.filter(look__slug=look_slug):
-                cache.delete(reverse('look-embed-identifier', args=[look_embed.identifier, look_slug]))
+                key = reverse('look-embed-identifier', args=[look_embed.identifier, look_slug])
+                cache.delete(key)
+                print 'Deleting %s' % key
