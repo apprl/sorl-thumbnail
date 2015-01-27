414f22dee63a70751d06dd5eb47a13330dbc850b
zklawik <klas.wikblad@heimore.com>
2015-01-27 09:59:44 +0100

    Fix for extracting forwarded ip from load balancer

--- apparelrow/statistics/utils.py
+++ apparelrow/statistics/utils.py
@@ -8,7 +8,7 @@ def get_client_referer(request, default=None):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[-1].strip()
+        ip = x_forwarded_for.split(',')[0].strip()
     else:
         ip = request.META.get('REMOTE_ADDR')
 
@@ -16,3 +16,5 @@ def get_client_ip(request):
 
 def get_user_agent(request):
     return request.META.get('HTTP_USER_AGENT', '')
+
+#def get_country_by_ip(request):
