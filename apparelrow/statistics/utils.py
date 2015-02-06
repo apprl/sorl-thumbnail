4eea4f7f70c85fbbd8e05594a3bc3d8d9eebcfa6
zklawik <klas.wikblad@heimore.com>
2015-02-06 13:27:24 +0100

    Adding timeout for requests object

--- apparelrow/statistics/utils.py
+++ apparelrow/statistics/utils.py
@@ -1,3 +1,6 @@
+from requests import Timeout
+
+
 def get_client_referer(request, default=None):
     referer = request.META.get('HTTP_REFERER')
     if not referer:
@@ -14,16 +17,25 @@ def get_client_ip(request):
 
     return ip
 
+
 def get_user_agent(request):
     return request.META.get('HTTP_USER_AGENT', '')
+
 
 def get_country_by_ip(request):
     import requests
     from apparelrow.settings import GEOIP_URL
     import logging
     log = logging.getLogger(__name__)
-    resp = requests.get(GEOIP_URL % get_client_ip(request))
-    json_obj = resp.json()
+    json_obj = None
+    try:
+        resp = requests.get(GEOIP_URL % get_client_ip(request),timeout=0.5)
+        json_obj = resp.json()
+    except Timeout,msg:
+        log.warning('Timeout occurred in geoip lookup function. > 500ms response time. Service down? [%s]' % msg)
+    except Exception,msg:
+        log.warning('Reply from geoip service not complient with json? [%s]' % msg)
+
     if hasattr(json_obj,"iso_code"):
         return json_obj.get("iso_code","ALL")
     else:
