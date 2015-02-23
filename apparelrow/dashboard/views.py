a38a176c8ad0df1796d3d49a01a05fb981726219
ebeni7ez <egbm91@gmail.com>
2015-02-23 12:19:27 +0100

    Removed unnecessary linebreaks

--- apparelrow/dashboard/views.py
+++ apparelrow/dashboard/views.py
@@ -1044,8 +1044,6 @@ def commissions(request):
         if request.COOKIES.get(settings.APPAREL_LOCATION_COOKIE, None) else request.session.get('location','ALL')
 
     vendors = get_available_stores(cookie_value)
-
-
     stores = list(get_model('dashboard', 'StoreCommission').objects.filter(vendor__name__in=vendors).select_related('vendor').order_by('vendor__name'))
     user_id = request.user.id
     stores = [store.calculated_commissions(store.commission, *get_cuts_for_user_and_vendor(user_id, store.vendor))
