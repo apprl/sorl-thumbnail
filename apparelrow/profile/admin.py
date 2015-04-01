6be59403c9d88e82d4ddf2e633480df9546834b6
Jasper <hahnjasper@googlemail.com>
2015-04-01 18:19:56 +0200

    removed notificationEvent admin.py

--- apparelrow/profile/admin.py
+++ apparelrow/profile/admin.py
@@ -9,7 +9,6 @@ from django.utils import timezone
 
 from apparelrow.profile.models import Follow
 from apparelrow.profile.models import NotificationCache
-from apparelrow.profile.models import NotificationEvent
 from apparelrow.profile.models import PaymentDetail
 
 
@@ -135,13 +134,6 @@ admin.site.unregister(Group)
 #
 
 admin.site.register(NotificationCache)
-
-class NotificationEventAdmin(admin.ModelAdmin):
-    raw_id_fields = ['owner', 'actor', 'look', 'product']
-    list_display = ('owner', 'actor', 'look', 'product', 'type', 'seen', 'email_sent')
-    list_filter = ('owner', 'actor')
-admin.site.register(NotificationEvent, NotificationEventAdmin)
-
 
 class FollowAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'modified')
