65d52416065147a52be27e94f25e0e3d00f8ab65
Jasper <hahnjasper@googlemail.com>
2015-04-10 10:44:01 +0200

    ordering looks by creation time

--- apparelrow/apparel/views/__init__.py
+++ apparelrow/apparel/views/__init__.py
@@ -631,18 +631,16 @@ def look_list(request, search=None, contains=None, gender=None):
         results = ApparelSearch(request.GET.get('q'), **query_arguments)
         queryset = queryset.filter(id__in=[doc.django_id for doc in results.get_docs()])
     elif view and view != 'all':
-       # logger.info("there is a view parameter for look list")
         if view == 'latest' or 'f' in request.GET:
             queryset = queryset.filter(published=True).filter(gender__in=gender_list.get(gender)).order_by('-created')
         elif view == 'friends':
             user_ids = []
             if is_authenticated:
                 user_ids = get_model('profile', 'Follow').objects.filter(user=request.user, active=True).values_list('user_follow_id', flat=True)
-                queryset = queryset.filter(gender__in=gender_list.get(gender)).filter(user__in=user_ids)
+                queryset = queryset.filter(gender__in=gender_list.get(gender)).filter(user__in=user_ids).order_by('-created')
     elif contains:
         queryset = queryset.filter(components__product__slug=contains).distinct()
     else:
-       # logger.info("using deault for gender %s" % gender)
         queryset = queryset.filter(gender__in=gender_list.get(gender)).order_by('-popularity', 'created')
 
 
