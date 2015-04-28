6c155599ea8ffc6b72fa3570091ff785db9dc456
ebeni7ez <egbm91@gmail.com>
2015-04-28 14:48:28 +0200

    Changed URL for Mailchimp webhook

--- apparelrow/apparel/urls.py
+++ apparelrow/apparel/urls.py
@@ -183,9 +183,9 @@ urlpatterns = patterns('',
     # Extra admin
 
     # Mailchimp - email
+    url(r'^mailchimp/webhook/$', 'apparelrow.apparel.email.mailchimp_webhook'),
     url(r'^admin/csv/users/$', 'apparelrow.apparel.email.admin_user_list_csv'),
     url(r'^admin/mail/weekly/$', 'apparelrow.apparel.email.generate_weekly_mail'),
-    url(r'^admin/mailchimp/webhook/$', 'apparelrow.apparel.email.mailchimp_webhook'),
     url(r'^admin/mail/custom/$', 'apparelrow.apparel.views.custom_email.admin', name='custom-email-admin'),
 
     url(r'^admin/dashboard/kpi/$', 'apparelrow.apparel.views.admin.kpi_dashboard', name='admin-kpi-dashboard'),
