e8fdd9c213a08c807f805708d1c950ebf435299d
ebeni7ez <emily@apprl.com>
2015-06-18 00:50:10 +0200

    added temporary url for the 'new dashboard'

--- apparelrow/dashboard/urls.py
+++ apparelrow/dashboard/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, url
+from apparelrow.dashboard.views import DashboardView
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', 'apparelrow.dashboard.views.index', name='index-publisher'),
     url(r'^apply/$', 'apparelrow.dashboard.views.publisher_contact', name='publisher-contact'),
+    url(r'^new_dashboard/(?P<year>\d{4})/(?P<month>\d{1,2})/$', DashboardView.as_view(), name='new-dashboard'),
+    url(r'^new_dashboard/$', DashboardView.as_view(), name='new-dashboard'),
     url(r'^dashboard/$', 'apparelrow.dashboard.views.dashboard', name='dashboard'),
     url(r'^referral/$', 'apparelrow.dashboard.views.referral', name='dashboard-referral'),
     url(r'^product/$', 'apparelrow.dashboard.views.products', name='dashboard-products'),
