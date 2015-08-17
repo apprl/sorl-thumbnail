8da2278e875c4c8957ef438380328a50617685f5
ebeni7ez <emily@apprl.com>
2015-08-17 15:23:45 +0200

    job that aggregates data handle different types of format for date input

--- apparelrow/dashboard/management/commands/collect_aggregated_data.py
+++ apparelrow/dashboard/management/commands/collect_aggregated_data.py
@@ -4,6 +4,7 @@ import logging
 from django.db.models.loading import get_model
 from django.contrib.auth import get_user_model
 import decimal
+import calendar
 from django.db.models import Count
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse
@@ -32,25 +33,38 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         Sale = get_model('dashboard', 'Sale')
         date = options.get('date')
-        if not date:
-            date = (datetime.date.today() - datetime.timedelta(1)).strftime('%Y-%m-%d')
-        query_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+        if date:
+            date_array = date.split("-")
+            year = int(date_array[0])
+            month = int(date_array[1])
 
-        start_date = datetime.datetime.combine(query_date, datetime.time(0, 0, 0, 0))
-        end_date = datetime.datetime.combine(query_date, datetime.time(23, 59, 59, 999999))
+            if len(date_array) > 2:
+                start_date_query = datetime.datetime.strptime(date, '%Y-%m-%d')
+                end_date_query = start_date_query
+            else:
+                start_date_query = datetime.date(year, month, 1)
+                end_date_query = start_date_query
+                end_date_query = end_date_query.replace(day=calendar.monthrange(start_date_query.year, start_date_query.month)[1])
+        else:
+            start_date_query = (datetime.date.today() - datetime.timedelta(1)).strftime('%Y-%m-%d')
+            start_date_query = datetime.datetime.strptime(start_date_query, '%Y-%m-%d')
+            end_date_query = start_date_query
+
+        start_date = datetime.datetime.combine(start_date_query, datetime.time(0, 0, 0, 0))
+        end_date = datetime.datetime.combine(end_date_query, datetime.time(23, 59, 59, 999999))
 
         # Get all earnings for the given date
         earnings = get_model('dashboard', 'UserEarning').objects.filter(date__range=(start_date, end_date))
 
         # Remove all existent data for the given date
-        get_model('dashboard', 'AggregatedData').objects.filter(date=query_date.date()).delete()
+        get_model('dashboard', 'AggregatedData').objects.filter(date__range=(start_date, end_date)).delete()
 
         for row in earnings:
             user_id, user_name, user_username = get_user_attributes(row.user)
             earning_amount = decimal.Decimal(row.amount)
 
             instance, created = get_model('dashboard', 'AggregatedData').objects.\
-            get_or_create(user_id=user_id, date=query_date.date(), type='aggregated_from_total',
+            get_or_create(user_id=user_id, date=row.date.date(), type='aggregated_from_total',
                           user_name=user_name, user_username=user_username)
             # Aggregate earnings by type
             if row.user_earning_type in ('referral_sale_commission', 'referral_signup_commission'):
@@ -68,7 +82,6 @@ class Command(BaseCommand):
             elif row.user_earning_type == 'publisher_network_click_tribute':
                 instance.network_click_earnings += earning_amount
                 instance.total_network_earnings += earning_amount
-                #instance.paid_clicks += get_clicks_from_sale(row.sale) ????
             elif row.user_earning_type == 'publisher_sale_click_commission':
                 instance.click_earnings += earning_amount
                 instance.sale_plus_click_earnings += earning_amount
@@ -91,7 +104,7 @@ class Command(BaseCommand):
             # Aggregated data for products
             if row.from_product:
                 product_instance, product_created = get_model('dashboard', 'AggregatedData').objects.\
-                    get_or_create(user_id=user_id, date=query_date.date(), type='aggregated_from_product', aggregated_from_id=row.from_product.id)
+                    get_or_create(user_id=user_id, date=row.date.date(), type='aggregated_from_product', aggregated_from_id=row.from_product.id)
                 if product_created:
                     product_instance.user_image, product_instance.user_link = get_user_thumbnail_and_link(row.user)
                     product_instance.user_name = user_name
@@ -122,7 +135,7 @@ class Command(BaseCommand):
 
             if row.user_earning_type in ('publisher_network_tribute', 'publisher_network_click_tribute'):
                 publisher_instance, publisher_created = get_model('dashboard', 'AggregatedData').objects.\
-                    get_or_create(user_id=user_id, date=query_date.date(), type='aggregated_from_publisher',
+                    get_or_create(user_id=user_id, date=row.date.date(), type='aggregated_from_publisher',
                                   aggregated_from_id=row.from_user.id, user_name=user_name, user_username=user_username)
                 publisher_earning = get_model('dashboard', 'UserEarning').objects.\
                     get(user=row.from_user, date__range=(start_date, end_date), sale=row.sale)
@@ -139,7 +152,7 @@ class Command(BaseCommand):
 
                     if row.from_product:
                         product_instance, product_created = get_model('dashboard', 'AggregatedData').objects.\
-                    get_or_create(user_id=user_id, date=start_date.date(), type='aggregated_from_product',
+                    get_or_create(user_id=user_id, date=row.date.date(), type='aggregated_from_product',
                                   aggregated_from_id=row.from_product.id)
                         if product_created:
                             product_instance.user_image, product_instance.user_link = get_user_thumbnail_and_link(row.user)
@@ -171,11 +184,11 @@ class Command(BaseCommand):
 
         # Total clicks
         total_clicks = get_model('statistics', 'ProductStat').objects.\
-            filter(created__range=(start_date, end_date)).values('user_id').\
+            filter(created__range=(start_date, end_date)).values('user_id', 'created').\
             annotate(clicks=Count('user_id')).order_by('clicks')
         for row in total_clicks:
             instance, created = get_model('dashboard', 'AggregatedData').objects.\
-                get_or_create(user_id=row['user_id'], date=query_date.date(), type='aggregated_from_total')
+                get_or_create(user_id=row['user_id'], date=row['created'].date(), type='aggregated_from_total')
             if created and not (row['user_id'] == 0):
                 row_user = get_user_model().objects.get(id=row['user_id'])
                 instance.user_image, instance.user_link = get_user_thumbnail_and_link(row_user)
@@ -185,7 +198,7 @@ class Command(BaseCommand):
 
         # Aggregate ProductStat
         aggregated_product_stat = get_model('statistics', 'ProductStat').objects.\
-            filter(created__range=(start_date, end_date)).values('user_id', 'product', 'vendor').\
+            filter(created__range=(start_date, end_date)).values('user_id', 'product', 'vendor', 'created').\
             annotate(clicks=Count('product')).order_by('clicks')
 
         for row in aggregated_product_stat:
@@ -195,7 +208,7 @@ class Command(BaseCommand):
                 user = get_user_model().objects.get(id=row['user_id'])
                 if product:
                     product_instance, product_created = get_model('dashboard', 'AggregatedData').objects.\
-                        get_or_create(user_id=row['user_id'], date=start_date.date(), type='aggregated_from_product',
+                        get_or_create(user_id=row['user_id'], date=row['created'].date(), type='aggregated_from_product',
                                       aggregated_from_id=product.id, aggregated_from_name=product.product_name,
                                       aggregated_from_slug=product.slug)
 
@@ -209,14 +222,14 @@ class Command(BaseCommand):
                         try:
                             earning = get_model('dashboard', 'UserEarning').objects.get(user_id=row['user_id'],
                                                                               user_earning_type='publisher_sale_click_commission',
-                                                                              date__range=(start_date, end_date))
+                                                                              date=row['created'].date())
                             clicks_number = get_clicks_from_sale(earning.sale)
                             click_cost = earning.amount / clicks_number
                             clicks = decimal.Decimal(row['clicks'])
                             product_instance.click_earnings += click_cost * clicks
                             product_instance.sale_plus_click_earnings += click_cost * clicks
                         except get_model('dashboard', 'UserEarning').DoesNotExist:
-                            logger.warning("Click earning for user %s date %s does not exist" % (user.id, start_date.date()))
+                            logger.warning("Click earning for user %s date %s does not exist" % (user.id, row['created'].date()))
 
                     product_instance.total_clicks += decimal.Decimal(row['clicks'])
                     product_instance.save()
@@ -224,7 +237,7 @@ class Command(BaseCommand):
                     if user.owner_network:
                         owner = user.owner_network
                         ownerp_instance, ownerp_created = get_model('dashboard', 'AggregatedData').objects.\
-                            get_or_create(user_id=owner.id, date=start_date.date(), type='aggregated_from_product',
+                            get_or_create(user_id=owner.id, date=row['created'].date(), type='aggregated_from_product',
                                       aggregated_from_id=product.id, aggregated_from_name=product.product_name,
                                       aggregated_from_slug=product.slug)
                         if ownerp_created:
@@ -238,14 +251,14 @@ class Command(BaseCommand):
                                 earning = get_model('dashboard', 'UserEarning').objects.get(user_id=owner.id,
                                                                                   user_earning_type='publisher_network_click_tribute',
                                                                                   from_user=user,
-                                                                                  date__range=(start_date, end_date))
+                                                                                  date=row['created'].date())
                                 clicks_number = get_clicks_from_sale(earning.sale)
                                 click_cost = earning.amount / clicks_number
                                 clicks = decimal.Decimal(row['clicks'])
                                 ownerp_instance.click_earnings += click_cost * clicks
                                 ownerp_instance.sale_plus_click_earnings += click_cost * clicks
                             except get_model('dashboard', 'UserEarning').DoesNotExist:
-                                logger.warning("Click earning for user %s date %s does not exist" % (owner.id, start_date.date()))
+                                logger.warning("Click earning for user %s date %s does not exist" % (owner.id, row['created'].date()))
 
                         ownerp_instance.total_clicks += decimal.Decimal(row['clicks'])
                         ownerp_instance.save()
