08171f901f80132c3f6107370bbfaa2fa69b22b7
zklawik <klas.wikblad@heimore.com>
2015-11-23 15:12:58 +0100

    Removal of push jobs in project. Moved to spiders

--- spiderpig/push_jobs.py
+++ spiderpig/push_jobs.py
@@ -15,9 +15,9 @@ import datetime
 
 crawl_group = datetime.datetime.today().day % 2
 
-suspended_group = ['qvc','menlook','nastygal','nelly-no','wolfnbadger','my-wardrobe','aldo','belstaff','bjornborg',
-                   'laurenb','monica-vinander','panos-emporio']
-priority_q = ['eleven','nelly','luisaviaroma','room21-no','rum21-se','ssense',
+suspended_group = ['dagmar','qvc','menlook','nastygal','nelly-no','wolfnbadger','my-wardrobe','aldo','belstaff','bjornborg',
+                   'laurenb','monica-vinander','panos-emporio','ssense',]
+priority_q = ['mr-porter','eleven','nelly','luisaviaroma','room21-no','rum21-se',
               'the-outnet','confident-living','net-a-porter','boozt-no','boozt-se','asos']
 cleaned_group = [spider for spider in spiders if spider not in priority_q]
 spiders = cleaned_group
