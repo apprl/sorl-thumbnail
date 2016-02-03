c312416f1fa54727bbaa854eb83c225dfa5dd879
zklawik <klas.wikblad@heimore.com>
2016-02-03 13:31:19 +0100

    Changed the jquery calls for changing selection options in the shop

--- apparelrow/static/js/browse.js
+++ apparelrow/static/js/browse.js
@@ -129,8 +129,8 @@ $(document).ready(function() {
 
     if (!!(window.history && history.pushState)) {
         $(document).on('click', '#product-gender a', function(e) {
-            var x = $('.navbar-nav-main > li > a[data-shop^="true"]');
-            var y = $('.navbar .navbar-form[data-search^="true"]');
+            var x = $('#menu').find('> ul > li > a[data-shop^="true"]');
+            var y = $('#search[data-search^="true"]');
             var gender = $(this).data('gender');
             if (gender == 'M') {
                 x.attr('href', x.attr('href').replace('women', 'men'));
