1b2cc2b48b9478c59459ad1d734205689b90ca3e
zklawik <klas.wikblad@heimore.com>
2016-02-15 13:58:43 +0100

    Fix for request user not existing in request object yet

--- apparelrow/apparel/middleware.py
+++ apparelrow/apparel/middleware.py
@@ -81,7 +81,7 @@ class LocationMiddleware(object):
 
     def process_response(self, request, response):
         cookie_value = request.COOKIES.get(settings.APPAREL_LOCATION_COOKIE, None)
-        is_auth = request.user.is_authenticated()
+        is_auth = request.user.is_authenticated() if hasattr(request, 'user') else False
         if is_auth:
             try:
                 if has_user_location(request):
