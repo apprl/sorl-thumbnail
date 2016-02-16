0d2196b2ac3844eec15edab0db0d01abc6cecd8d
zklawik <klas.wikblad@heimore.com>
2016-02-16 09:37:22 +0100

    Import statement for get_location missing

--- apparelrow/apparel/views/__init__.py
+++ apparelrow/apparel/views/__init__.py
@@ -42,7 +42,7 @@ from apparelrow.apparel.models import Look, LookLike, ShortProductLink, ShortSto
 from apparelrow.apparel.models import get_cuts_for_user_and_vendor
 from apparelrow.apparel.search import ApparelSearch, more_like_this_product, more_alternatives
 from apparelrow.apparel.utils import get_paged_result, vendor_buy_url, get_featured_activity_today, \
-    select_from_multi_gender, JSONResponse, JSONPResponse, shuffle_user_list
+    select_from_multi_gender, JSONResponse, JSONPResponse, shuffle_user_list, get_location
 from apparelrow.apparel.tasks import facebook_push_graph, facebook_pull_graph, look_popularity, build_static_look_image
 from apparelrow.activity_feed.views import user_feed
 from apparelrow.dashboard.views import SignupForm
