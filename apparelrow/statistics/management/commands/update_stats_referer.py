ad3c08ed8d229a19e67bb679ea84912ebd75ab0a
zklawik <klas.wikblad@heimore.com>
2016-05-03 13:20:47 +0200

    Split intervalls of batch jobs

--- apparelrow/statistics/management/commands/update_stats_referer.py
+++ apparelrow/statistics/management/commands/update_stats_referer.py
@@ -9,13 +9,18 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         #product_stats = ProductStat.objects.all()
-        pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=ProductStat.objects.count()).start()
-        for index, row in enumerate(ProductStat.objects.all()):
-            pbar.update(index)
-            if row.referer:
-                referer_array = row.referer.split('\n')
+        steps = 10000
+        position = 0
+        for i in range(steps, 2000000, steps):
+            products = ProductStat.objects.all()[position:i]
+            pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=products.count()).start()
+            for index, row in enumerate(products):
+                pbar.update(index)
+                if row.referer:
+                    referer_array = row.referer.split('\n')
 
-                if len(referer_array) > 0:
-                    row.referer = referer_array[0]
-                    row.save()
-        pbar.finish()
+                    if len(referer_array) > 0:
+                        row.referer = referer_array[0]
+                        row.save()
+            pbar.finish()
+            position = i
