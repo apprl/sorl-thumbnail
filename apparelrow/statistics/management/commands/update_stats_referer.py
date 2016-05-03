0ebf17fe92c13d1a06e337915632e33a3fa3cfb7
zklawik <klas.wikblad@heimore.com>
2016-05-03 12:28:12 +0200

    Fix for update stats job timing out

--- apparelrow/statistics/management/commands/update_stats_referer.py
+++ apparelrow/statistics/management/commands/update_stats_referer.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from progressbar import ProgressBar, Percentage, Bar
 from apparelrow.statistics.models import ProductStat
 
 
@@ -7,12 +8,14 @@ class Command(BaseCommand):
     help = 'Update referer for ProductStat instances, so it will only include HTTP referer link and not any other'
 
     def handle(self, *args, **options):
-        product_stats = ProductStat.objects.all()
-
-        for row in product_stats:
+        #product_stats = ProductStat.objects.all()
+        pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=ProductStat.objects.count()).start()
+        for index, row in enumerate(ProductStat.objects.all()):
+            pbar.update(index)
             if row.referer:
                 referer_array = row.referer.split('\n')
 
                 if len(referer_array) > 0:
                     row.referer = referer_array[0]
                     row.save()
+        pbar.finish()
