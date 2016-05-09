f0a9504f34e53895c9a61719767c01e20265abe3
ebeni7ez <emily@apprl.com>
2016-05-09 10:36:37 +0200

    Fixed Blog posts to make it faster and simple pagination included

--- apparelrow/statistics/management/commands/update_stats_referer.py
+++ apparelrow/statistics/management/commands/update_stats_referer.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from apparelrow.statistics.models import ProductStat
 
+from progressbar import ProgressBar, Percentage, Bar
+
 
 class Command(BaseCommand):
     args = ''
@@ -8,6 +10,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         product_stats = ProductStat.objects.all()
+        count = 0
+
+        # Initialize progress bar
+        pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=len(product_stats)).start()
 
         for row in product_stats:
             if row.referer:
@@ -16,3 +22,7 @@ class Command(BaseCommand):
                 if len(referer_array) > 0:
                     row.referer = referer_array[0]
                     row.save()
+                pbar.update(count)
+                count += 1
+
+        pbar.finish()
