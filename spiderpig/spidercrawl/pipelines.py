865ed9551cba952cec20cbb36eae9bcda6784c1c
zklawik <klas.wikblad@heimore.com>
2016-05-18 16:04:39 +0200

    Removed non allowed field verb in ProductItem

--- spiderpig/spidercrawl/pipelines.py
+++ spiderpig/spidercrawl/pipelines.py
@@ -154,8 +154,7 @@ class DatabaseHandler:
         if bool(created or updated):
             cache.set(self.scraped_cache_key.format(id=product.id), product_hash, 3600*24*90)
             verb = "updated" if updated else "created"
-            item.update({"verb": verb})
-            log.info('Product {key} is {verb}, parsing will ensue.'.format(**item))
+            log.info('Product {key} is {verb}, parsing will ensue.'.format(key=item["key"], verb=verb))
             if ASYNC_PARSING:
                 parse_theimp_product.delay(product.id)
             else:
