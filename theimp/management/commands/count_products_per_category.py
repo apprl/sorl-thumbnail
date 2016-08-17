7b03d9c4c2a19e969596c2ebd2e71c0848fa4635
zklawik <klas.wikblad@heimore.com>
2016-08-17 15:46:33 +0200

    Changed filtering to only include products which has been updated within the last 60 days.

--- theimp/management/commands/count_products_per_category.py
+++ theimp/management/commands/count_products_per_category.py
@@ -1,23 +1,50 @@
 import logging
+import optparse
 import time
 from django.core.management.base import BaseCommand
 from django.db.models.loading import get_model
+from datetime import date, timedelta
+from progressbar import ProgressBar, Percentage, Bar
 
 logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
+    """
+    Sidenote, this job is for some reason not attached to the scheduler. Its run by crontab by user deploy on server admin.
+    """
+
     args = ''
     help = 'Counts products for every parsed category'
+    quarantine = 60
+    option_list = BaseCommand.option_list + (
+        optparse.make_option('--verbose',
+            action='store_true',
+            dest='verbose',
+            help='Creating verbose progressbar output.',
+            default= False,
+        ),
+    )
 
     def handle(self, *args, **options):
         logger.debug("Starting counting...")
+        modified_since = date.today() - timedelta(days=self.quarantine)
         start = time.time()
-        for category in self.get_category_mappings():
-            products_counter = get_model('theimp', 'Product').objects.filter(category_mapping=category).count()
+        pbar = None
+        if options.get("verbose"):
+            maxval = get_model('theimp', 'CategoryMapping').objects.count()
+            pbar = ProgressBar(widgets=[Percentage(), Bar()], maxval=maxval).start()
+
+        for index, category in enumerate(self.get_category_mappings()):
+            if pbar:
+                pbar.update(index)
+
+            products_counter = get_model('theimp', 'Product').objects.filter(category_mapping=category, modified__gt=modified_since).count()
             if category.products_counter != products_counter:
                 category.products_counter = products_counter
                 category.save()
+        if pbar:
+            pbar.finish()
         end = time.time()
         logger.debug("Finishing on %s seconds"%(end - start))
 
