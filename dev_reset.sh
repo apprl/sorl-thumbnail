d7f9ed0dc500e3f2c20cb3102184e0ed63a76422
mkotsalainen <matti@kotsalainen.org>
2017-01-20 21:24:11 +0000

    Small changes to dev_reset.sh, made databse dump a little smaller

--- dev_reset.sh
+++ dev_reset.sh
@@ -1,4 +1,12 @@
-#!/usr/bin/env bash
+#! /bin/bash
+
+green=$'\e[1;32m'
+end=$'\e[0m'
+
+function echo_green {
+    printf "%s\n" "${green}$1${end}"
+}
+
 echo "This command will remove all data in all your running docker containers!"
 echo "Docker containers will be recreated and data will be restored from apprl_db_backup.sql."
 echo "The database will also be repopulated and reindexed."
@@ -12,10 +20,9 @@ then
     exit 1
 fi
 
-pip install -r etc/requirements.dev.pip &
+echo_green "Setting up docker"
 
 project_path=./devops/docker/apprl
-
 docker-compose -f $project_path/docker-compose.yml down
 docker-compose -f $project_path/docker-compose.yml up --force-recreate -d
 
@@ -26,33 +33,36 @@ then
 fi
 
 
-echo "Get development database backup from s3"
+echo_green "Get development database backup from s3"
 aws s3 sync s3://apprl-vagrant/dev_db_backups/ devops/dev_db_backups/.
 
-echo "Restore database apprl_db_backup.sql"
+echo_green "Creating database, we'll get errors until postgres is initialized"
 until createdb apparel -hlocalhost -Upostgres
 do
-    echo "Retry create db in two seconds, postgres probably still starting up"
-    sleep 2
+    sleep 1
 done
 
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d apparel ./devops/dev_db_backups/latest_dev_backup.sql
+# We have two database dumps:
+# latest_small_dev_backup.sql
+# latest_dev_backup.sql - same as small dump, but contains productstats, products etc.
 
-echo "Migrate database"
+echo_green "Restore database apprl_db_backup.sql"
+pg_restore --clean --if-exists --verbose --no-acl --no-owner -h localhost -U postgres -d apparel ./devops/dev_db_backups/latest_small_dev_backup.sql
+
+
+echo_green "Install requirements"
+pip install -r etc/requirements.dev.pip &
+
+echo_green "Migrate database"
 ./manage.py migrate
 
-echo "Delete all product data to later repopulate it"
-echo 'from apparelrow.dashboard.models import UserEarning; UserEarning.objects.all().delete()' | ./manage.py shell
-echo 'from apparelrow.apparel.models import Product; Product.objects.all().delete()' | ./manage.py shell
-
-echo "Import data from shirtonomy"
+echo_green "Import data from shirtonomy"
 cd spiderpig
 scrapy crawl shirtonomy
 cd -
 
-echo "Reindex data to make sure all is up-to-date"
-./manage.py rebuild_index --clean --model=product
-# if rebuild index craches on images try running  ./manage.py thumbnail clear first
+echo_green "Reindex data to make sure all is up-to-date"
+./manage.py rebuild_index --clean
 
 # echo "Run tests"
 # ./manage.py test
