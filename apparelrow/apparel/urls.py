faa747859e0e1f5b5ef281999f1179caec8d35b9
mkotsalainen <matti@kotsalainen.org>
2017-02-14 21:29:23 +0000

    Maded looks2 not be the default implementation of the looks page

--- apparelrow/apparel/urls.py
+++ apparelrow/apparel/urls.py
@@ -198,7 +198,7 @@ urlpatterns = patterns('',
     url(r'^look/(?P<pk>\d+)/?$', LookView.as_view(), name='look'),
 
     url(r'^looks/$', 'apparelrow.apparel.views.look_list', {'gender': 'A'}, name='look-list'),
-    url(r'^looks2/$', 'apparelrow.apparel.views.look_list_popularity', {'gender': 'A'}, name='look-list'),
+    url(r'^looks2/$', 'apparelrow.apparel.views.look_list_popularity', {'gender': 'A'}, name='look-list-2'),
     url(r'^looks/men/$', 'apparelrow.apparel.views.look_list', {'gender': 'M'}, name='look-list-men'),
     url(r'^looks/women/$', 'apparelrow.apparel.views.look_list', {'gender': 'W'}, name='look-list-women'),
     url(r'^looks/search/$', 'apparelrow.apparel.views.look_list', {'search': True}, name='search-look-list'),
