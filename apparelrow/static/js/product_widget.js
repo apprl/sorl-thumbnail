d2945cddea695b1074ab2d26fc5c70f33e9ab170
Karl Alnebratt <karl.alnebratt@gmail.com>
2017-08-15 14:06:04 +0200

    Merge for productwidget 1

--- apparelrow/static/js/product_widget.js
+++ apparelrow/static/js/product_widget.js
@@ -28,14 +28,14 @@ jQuery(document).ready(function() {
     var $items = $ul.children('li.product');
     var index = 0;
     var running = false;
-    var visiblechildren;
-    var doslide = false;
+    var visibleChildren;
+    var doSlide = false;
     var $container = $('.slidecontainer');
-    var padding = 20;
-    var $slidenext = $('.next');
-    var $slideprevious = $('.previous');
-    var itemwidth = 0;
-    var slidefactor = 1;
+    var navigationWidth = 60;
+    var $slideNext = $('.next');
+    var $slidePrevious = $('.previous');
+    var itemWidth = 0;
+    var slideFactor = 1;
     var $lazyImgs = $('a.product img');
 
     function slide(direction) {
@@ -44,118 +44,121 @@ jQuery(document).ready(function() {
         if (embed_type == 'single') {
             index -= direction;
         } else {
-            slidefactor = visiblechildren*2 <= $items.length ? visiblechildren : $items.length - visiblechildren;
-            index -= slidefactor*direction;
+            slideFactor = visibleChildren*2 <= $items.length ? visibleChildren : $items.length - visibleChildren;
+            index -= slideFactor*direction;
         }
         if (index < 0) {
             for (var i=0;i<-1*index;i++) {
                 $ul.children('li').last().detach().prependTo($ul);
             }
-            $ul.css('left', -1*slidefactor*itemwidth + 'px');
+            $ul.css('left', -1*slideFactor*itemWidth + 'px');
             // adjust index
             index = 0;
             $items = $ul.children('li');
-        } else if (index + visiblechildren > $items.length || ($ul.width() + direction*itemwidth*slidefactor + $ul.position().left < $container.width())) {
-            var diff = index+visiblechildren-$items.length;
+        } else if (index + visibleChildren > $items.length || ($ul.width() + direction*itemWidth*slideFactor + $ul.position().left < $container.width())) {
+            var diff = index+visibleChildren-$items.length;
             for (var i=0;i<diff;i++) {
                 $ul.children('li').first().detach().appendTo($ul);
             }
-            $ul.css('left', $ul.position().left + diff*itemwidth + 'px');
+            $ul.css('left', $ul.position().left + diff*itemWidth + 'px');
             // adjust index
-            index = $items.length - visiblechildren;
+            index = $items.length - visibleChildren;
             $items = $ul.children('li');
         }
-        $ul.animate({'left': '+=' + slidefactor*direction*itemwidth}, {'complete': function() { running = false; $lazyImgs = loadImgs($lazyImgs) }});
+        $ul.animate({'left': '+=' + slideFactor*direction*itemWidth}, {'complete': function() { running = false; $lazyImgs = loadImgs($lazyImgs) }});
     }
 
     var mc = new Hammer($('.slidecontainer')[0]);
-    mc.on('swipeleft swiperight', function(e) { if (!doslide) return; slide(e.type == 'swipeleft' ? -1 : 1); });
+    mc.on('swipeleft swiperight', function(e) { if (!doSlide) return; slide(e.type == 'swipeleft' ? -1 : 1); });
 
-    function enableslide() {
-        if (!doslide) {
-            $slideprevious.on('click', function () {
+    function enableSlide() {
+        if (!doSlide) {
+            $slidePrevious.on('click', function () {
                 slide(1);
             });
-            $slidenext.on('click', function () {
+            $slideNext.on('click', function () {
                 slide(-1);
             });
         }
-        $slidenext.parent().show();
-        doslide = true;
+        $slideNext.parent().show();
+        doSlide = true;
     }
 
-    function disableslide() {
+    function disableSlide() {
         $('.previous').off('click');
         $('.next').off('click').parent().hide();
-        doslide = false;
+        doSlide = false;
     }
 
     function resize() {
         var $window = $(window);
+        var winWidth = $window.width() - navigationWidth;
         var $this;
-        var imgratio;
-        var maxheight = 0;
+        var imgRatio;
+        var maxHeight = 0;
         var $images = $('a.product img');
-        var childwidth = 0;
-        var margin = 120;
-        var captionheight = $('.caption').length ? $('.caption').first().height() + 10 : 0;
+        var childWidth = 0;
+        var padding = 10;
+        var captionHeight = $('.caption').length ? $('.caption').first().height() + 10 : 0;
+        var numItemsPerSlide = winWidth > 1450 ? 8 : winWidth > 1160 ? 6 : winWidth > 870 ? 5 : winWidth > 580 ? 4 : winWidth > 400 ? 3 : 2;
 
-        $('.slidecontainer, .productcontainer').css({height: 'auto', width: $window.width() + (padding * 2)});
+        if (embed_type === 'single') {
+            numItemsPerSlide = 1;
+        }
+
+        itemWidth = Math.round(winWidth / numItemsPerSlide);
+        $items.width(itemWidth)
+        $ul.width($items.length * itemWidth);
+        $container.css({height: 'auto', width: winWidth});
 
         $images.each(function(item, i) {
             $this = $(this);
-            imgratio = $this.attr('width')/$this.attr('height');
-            if (imgratio > 1 && $window.width()/$window.height() < 1) {
+            imgRatio = $this.attr('width')/$this.attr('height');
+            if (imgRatio > 1 && $window.width()/$window.height() < 1) {
                 if (width > $window.width()) width = $window.width();
                 $this.css({'width': width, height: Math.round($(this).attr('height')/$(this).attr('width')*width)});
             } else {
-                $this.css({height: $window.height() - captionheight, width: Math.round($(this).attr('width')/$(this).attr('height')*($window.height()-captionheight))});
+                var newHeight = $window.height() - captionHeight;
+                var newWidth = Math.round($(this).attr('width')/$(this).attr('height') * newHeight) - padding;
+                $this.css({height: 'auto', width: newWidth });
             }
-            maxheight = Math.max(maxheight, $this.height());
-            childwidth = Math.max(childwidth, $this.width());
+            maxHeight = Math.max(maxHeight, $this.height());
+            childWidth = Math.max(childWidth, $this.width());
         });
 
-        if ((childwidth + margin + padding) > $window.width()) {
-            var width = ($window.width() - margin - padding);
-            imgratio = $this.attr('width')/$this.attr('height');
-            width = (width/imgratio - captionheight) * imgratio;
+        if ((childWidth) > $window.width()) {
+            var width = ($window.width());
+            imgRatio = $this.attr('width')/$this.attr('height');
+            width = (width/imgRatio - captionHeight) * imgRatio;
             $images.each(function(item, i) {
                 $(this).css({width: width, height: 'auto'});
             });
-            childwidth = width;
+            childWidth = width;
         }
-
-        itemwidth = $items.first().width() + padding;
 
         if (embed_type == 'single') {
-            $container.width(itemwidth);
+            $container.width(itemWidth);
             if ($items.length > 1) {
-                enableslide();
+                enableSlide();
             } else {
-                disableslide();
+                disableSlide();
             }
-            visiblechildren = 1;
+            visibleChildren = 1;
         } else {
-            visiblechildren = Math.floor(($window.width() - margin)/(itemwidth));
-            $container.width(visiblechildren*(itemwidth));
-            if (Math.floor(visiblechildren) < $items.length) {
-                enableslide();
+            visibleChildren = Math.floor(($window.width())/(itemWidth));
+            $container.width(visibleChildren*(itemWidth));
+            if (Math.floor(visibleChildren) < $items.length) {
+                enableSlide();
             } else {
-                disableslide()
+                disableSlide()
             }
         }
-        $ul.children('li').css('width', itemwidth);
         index = 0;
         $ul.css('left', 0);
-        var containermargin = parseInt($container.css('marginLeft').substr(0, $container.css('marginLeft').length -2));
-        $slideprevious.css({left: Math.max(10, containermargin-$slideprevious.width() - 10)});
-        $slidenext.css({right: Math.max(10, containermargin-$slideprevious.width() - 10)});
-        // Set width of list
-        $ul.width($items.length * itemwidth);
         $lazyImgs = loadImgs($lazyImgs)
     }
 
-    enableslide();
+    enableSlide();
     $(window).on('resize', resize);
     resize();
 
