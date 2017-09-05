d7ba11657c9c8a3f301c586d34a4b747485c2f70
dadoun <mona.dadoun@gmail.com>
2017-09-05 14:01:18 +0200

    Add admin for URL model

--- product_match/admin.py
+++ product_match/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from product_match.models import Url
+
 
 # Register your models here.
+
+class UrlAdmin(admin.ModelAdmin):
+    ordering = ('-created',)
+    readonly_fields = ('created', 'modified')
+    list_display = ('domain', 'path', 'query', 'fragmens', 'parameters')
+    list_filter = ('domain', 'path')
+    raw_id_fields = ('product',)
+
+
+admin.site.register(Url, UrlAdmin)
--- product_match/models.py
+++ product_match/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from apparelrow.apparel.models import Product
+from django.utils.translation import get_language, ugettext_lazy as _
+
 
 # Create your models here.
+class Url(models.Model):
+    product = models.ForeignKey(Product, related_name='url_products', blank=False, null=False)
+    url = models.CharField(max_length=512, null=True, blank=True)
+    domain = models.CharField(max_length=500, null=True, blank=True)
+    path = models.CharField(max_length=500, null=True, blank=True)
+    query = models.CharField(max_length=500, null=True, blank=True)
+    fragment = models.CharField(max_length=505, null=True, blank=True)
+    parameters = models.CharField(max_length=500, null=True, blank=True)
+    created = models.DateTimeField(_("Time added"), null=True, blank=True, db_index=True)
+    modified = models.DateTimeField(_("Time modified"), null=True, auto_now=True)
