6b70e60bfc3a2a98843b8539561bbd84d538e3df
dadoun <mona.dadoun@gmail.com>
2017-09-06 11:24:02 +0200

    Change URL name to URLDETAIL

--- product_match/models.py
+++ product_match/models.py
@@ -2,15 +2,14 @@ from django.db import models
 
 #import theimp
 from apparelrow.apparel.models import Product
-from django.utils.translation import get_language, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 
 # Create your models here.
-class Url(models.Model):
-    product = models.ForeignKey("apparel.Product", null=True, blank=True)
-    product = models.ForeignKey('theimp.Product.product', related_name='url_products', blank=False, null=False)
-    url = models.CharField(max_length=512, null=True, blank=True)
-    domain = models.CharField(max_length=500, null=True, blank=True)
+class UrlDetail(models.Model):
+    product = models.ForeignKey(Product, null=False, blank=False)
+    url = models.URLField(max_length=512, null=False, blank=False)
+    domain = models.CharField(max_length=512, null=False, blank=False)
     path = models.CharField(max_length=500, null=True, blank=True)
     query = models.CharField(max_length=500, null=True, blank=True)
     fragment = models.CharField(max_length=505, null=True, blank=True)
