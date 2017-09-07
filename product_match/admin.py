d3d80576d2807333c9a9ce812e2bb91572bc5b09
dadoun <mona.dadoun@gmail.com>
2017-09-07 17:47:34 +0200

    Add admin for new model

--- product_match/admin.py
+++ product_match/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from product_match.models import UrlDetail
+from product_match.models import UrlDetail, UrlVendorSpecificParams
 
 
 # Register your models here.
 
-class UrlAdmin(admin.ModelAdmin):
+class UrlDetailAdmin(admin.ModelAdmin):
     ordering = ('-created',)
     readonly_fields = ('created', 'modified')
     list_display = ('domain', 'path', 'query', 'fragment', 'parameters')
@@ -12,4 +12,9 @@ class UrlAdmin(admin.ModelAdmin):
     raw_id_fields = ('product',)
 
 
-admin.site.register(UrlDetail, UrlAdmin)
+admin.site.register(UrlDetail, UrlDetailAdmin)
+
+class UrlVendorSpecificParamsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UrlVendorSpecificParams, UrlVendorSpecificParamsAdmin)
