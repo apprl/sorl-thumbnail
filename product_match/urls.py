4e13b8d6102fc50cd8c16395686f4c41be2954ff
dadoun <mona.dadoun@gmail.com>
2017-09-08 15:40:26 +0200

    Fix route

--- product_match/urls.py
+++ product_match/urls.py
@@ -1,3 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import  url
+from product_match.views import create_short_links_and_lookup_product
 
-url(r'^backend/product/lookup/$', 'apparelrow.apparel.views.lookup_product', name='product-lookup'),
+url(r'^backend/product/lookup/match/product/$', create_short_links_and_lookup_product,
+    name='backend-product-lookup-match-product')
