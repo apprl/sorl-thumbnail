d3fc03cd37a0b5f597805082dcdd25907eb8047d
dadoun <mona.dadoun@gmail.com>
2017-09-08 15:38:49 +0200

    Change parameter name in models

--- product_match/models.py
+++ product_match/models.py
@@ -13,12 +13,12 @@ class UrlDetail(models.Model):
     path = models.CharField(max_length=500, null=True, blank=True)
     query = models.CharField(max_length=500, null=True, blank=True)
     fragment = models.CharField(max_length=505, null=True, blank=True)
-    # split the parameters and add them to json
-    parameters = models.CharField(max_length=500, null=True, blank=True)
     created = models.DateTimeField(_("Time added"), null=True, blank=True, db_index=True)
     modified = models.DateTimeField(_("Time modified"), null=True, auto_now=True)
 
 
 class UrlVendorSpecificParams(models.Model):
     domain = models.CharField(max_length=512, null=False, blank=False)
-    param_id_name = models.CharField(max_length=512, null=False, blank=False)
+    parameter_name = models.CharField(max_length=512, null=False, blank=False)
+    created = models.DateTimeField(_("Time added"), null=True, blank=True, db_index=True)
+    modified = models.DateTimeField(_("Time modified"), null=True, auto_now=True)
